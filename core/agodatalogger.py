25a44e7c058098822e25328c8f4f5a3ee4eceeea
Christoph Jaeger <christoph@agocontrol.com>
2012-12-17 21:10:57 +0000

    get graph via json alpha test - test with command getloggergraph and look at console

--- core/agodatalogger.py
+++ core/agodatalogger.py
@@ -20,6 +20,10 @@ from pysqlite2 import dbapi2 as sqlite3
 import sqlite3 as lite
 import datetime
 
+import pandas
+import pandas.io.sql as psql
+import simplejson
+
 config = ConfigParser.ConfigParser()
 config.read('/etc/opt/agocontrol/config.ini')
 
@@ -59,6 +63,56 @@ sys.stderr = LogErr()
 # get sqlite connection
 con = lite.connect('agodatalogger.db')
 
+
+#def GetGraphData(uuid, start, end):
+def GetGraphData():
+	#uuid = uuid
+	#start_date = start
+	#end_date = end
+
+	uuid="5ee20e88-8dd7-451f-a2c7-00ead37f2216"
+	start_date="2012-11-13 11:00:00"
+	end_date="2012-11-13 11:15:00"
+	environment="temperature"
+
+	try:
+		df = psql.read_frame("""SELECT strftime('%s', timestamp) AS Date,
+		environment AS Env,
+		unit AS Unit,
+		AVG(level) AS Level
+		FROM data
+		WHERE timestamp BETWEEN '""" + start_date + """' AND '""" + end_date + """' 
+		AND environment='""" + environment + """'
+		AND uuid='""" + uuid + """' GROUP BY strftime('%s', timestamp), Unit
+		ORDER BY timestamp""", con, index_col = 'Date')
+
+		if not df.empty:
+			df.index = [pandas.datetools.to_datetime(datetime.datetime.fromtimestamp(int(di)).strftime('%Y-%m-%d %H:%M:%S')) for di in df.index]
+
+			ticks = df.ix[:, ['Level']]
+
+			ticks = ticks.asfreq('1Min', method='pad').prod(axis=1).resample('5min', how='mean')
+
+			start_date = datetime.datetime(2012, 11, 13, 11, 00, 0, 0)
+			end_date = datetime.datetime(2012, 11, 13, 14, 30, 00, 0)
+			date_range = pandas.DatetimeIndex(start=start_date, end=end_date, freq='5Min')
+
+      			df2 = ticks.reindex(date_range).fillna(method='backfill').fillna(method='pad')
+			data = map(lambda x: x.strip(), str(df2).splitlines(True))
+			json_map = {}
+			for i in range(len(data) - 1):
+				json_map[data[i][:19]] = data[i][23:]
+			return simplejson.dumps(json_map)
+		else:
+			return "No data"
+
+	except sqlite3.Error as e:
+		print  "Error " + e.args[0]
+
+###
+
+
+
 connection = Connection(broker, username=username, password=password, reconnect=True)
 try:
 	connection.open()
@@ -88,7 +142,8 @@ try:
 
 			if 'command' in message.content:
 				if message.content['command'] == 'getloggergraph':
-					print "Got getloggergraph command"						
+					result = GetGraphData()
+					print result
 
 			session.acknowledge()
 
