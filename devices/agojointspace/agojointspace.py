2c7b07a58f288a866e39a8b53ba354e5c239e1de
foxi352 <foxi352@agocontrol.com>
2013-01-08 20:51:40 +0000

    Refs #59 - First checkin of agojointspace.py script. Does disconver jointspace devices an LAN and reports them to core. Commands not yet implemented

---
+++ devices/agojointspace/agojointspace.py
@@ -0,0 +1,232 @@
+#! /usr/bin/env python
+# JointSpace compatible devices - Philips TV sets > 2010, Blueray Players, ... (http://jointspace.sourceforge.net)
+
+import sys
+import syslog
+import pickle
+import optparse
+import ConfigParser
+
+from qpid.messaging import *
+from qpid.util import URL
+from qpid.log import enable, DEBUG, WARN
+
+import thread
+import time
+
+import select
+import urllib2
+import json
+import binascii
+import uuid
+
+from socket import *
+from struct import *
+
+# get this device UUID by using MAC address. Used only for discovery broadcasts to not recognize ourselves as player device
+myUUID = uuid.uuid1()
+
+config = ConfigParser.ConfigParser()
+config.read('/etc/opt/agocontrol/config.ini')
+
+try:
+	username = config.get("system","username")
+except ConfigParser.NoOptionError, e:
+	username = "agocontrol"
+
+try:
+	password = config.get("system","password")
+except ConfigParser.NoOptionError, e:
+	password = "letmein"
+
+try:
+	broker = config.get("system","broker")
+except ConfigParser.NoOptionError, e:
+	broker = "localhost"
+
+try:
+	debug = config.get("system","debug")
+except ConfigParser.NoOptionError, e:
+	debug = "WARN"
+
+try:
+	jointspacehost = config.get("jointspace","device")
+except:
+	jointspacehost = "localhost"
+
+try:
+	jointspaceport = int(config.get("jointspace","port"))
+except:
+	jointspaceport = 1925
+
+try:
+	voodooPort = int(config.get("jointspace","voodooport"))
+except:
+	voodooPort = 2323
+	
+if debug=="DEBUG":
+	enable("qpid", DEBUG)
+else:
+	enable("qpid", WARN)
+
+ 
+parser = optparse.OptionParser(usage="usage: %prog <command> [options] [ PARAMETERS ... ]",
+                               description="send automation control commands")
+parser.add_option("-b", "--broker", default=broker, help="hostname of broker (default %default)")
+parser.add_option("-u", "--username", default=username, help="specify a username")
+parser.add_option("-P", "--password", default=password, help="specify a password")
+
+opts, args = parser.parse_args()
+
+# route stderr to syslog
+class LogErr:
+	def write(self, data):
+		syslog.syslog(syslog.LOG_ERR, data)
+
+syslog.openlog(sys.argv[0], syslog.LOG_PID, syslog.LOG_DAEMON)
+# sys.stderr = LogErr()
+
+connection = Connection(opts.broker, username=opts.username, password=opts.password,  reconnect=True)
+connection.open()
+session = connection.session()
+receiver = session.receiver("agocontrol; {create: always, node: {type: topic}}")
+sender = session.sender("agocontrol; {create: always, node: {type: topic}}")
+
+def setDeviceName(deviceUUID, name):
+	try:
+		content = {}
+		content["command"] = "setdevicename"
+		content["uuid"] = deviceUUID
+		content["name"] = name
+		message = Message(content=content)
+		sender.send(message)
+	except SendError, e:
+		print e
+
+def reportdevice(deviceUUID, type, product):
+	try:
+		content = {}
+		content["devicetype"]=type
+		content["uuid"] = deviceUUID
+		content["product"] = product
+		message = Message(content=content,subject="event.device.announce")
+		sender.send(message)
+	except SendError, e:
+		print e
+
+# broadcast discovery packet to LAN
+def broadcast_discovery(frequency):
+	buffer = pack('!32sii16s96s96s96s', 'Test', 0x01000000,0x02000000,myUUID.bytes,'Agocontrol-jointSpace','Agocontrol','Agocontrol')
+	s = socket(AF_INET, SOCK_DGRAM)
+	try:
+		s.bind(('', 0))
+	except ReceiverError, e:
+		syslog.syslog(syslog.LOG_ERR, 'broadcast_discovery: failure to bind')
+		s.close()
+		raise	
+	s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+	time.sleep(2)
+	while True:
+		s.sendto(buffer, ('<broadcast>', voodooPort))
+		time.sleep(frequency)
+	
+# thread to listen for player info
+def player_info():
+	s = socket(AF_INET, SOCK_DGRAM)
+	tempUUID=uuid
+	try:
+		s.bind(('', voodooPort))
+	except ReceiverError, e:
+		syslog.syslog(syslog.LOG_ERR, 'player_info: failure to bind')
+		s.close()
+		raise
+	while True:
+		d = s.recvfrom(1024)
+		data = d[0]
+		addr = d[1]
+		message=unpack('!32sii16s96s96s96s', data)
+		# extract data
+		playerIP=addr[0]
+		playerUUID=str(tempUUID.UUID(bytes=message[3]))
+		playerName=message[4].strip('\0')
+		playerVendor=message[5].strip('\0')
+		playerModel=message[6].strip('\0')
+		if playerUUID != str(myUUID):
+			# log discovery
+			log="Discovered - IP:"+playerIP \
+				+", uuid:"+playerUUID \
+				+", name:"+playerName \
+				+", vendor:"+ playerVendor \
+				+", model:"+ playerModel
+			#print log # DEBUG
+			syslog.syslog(syslog.LOG_NOTICE, log)
+			# report to server and name device
+			reportdevice(playerUUID, "avreceiver", playerModel)
+			setDeviceName(playerUUID,playerName + " (" + playerVendor + " " + playerModel + ")")
+		
+syslog.syslog(syslog.LOG_NOTICE, "agocontrol jointspace device is starting up")
+
+# Create player info collector thread
+try:
+	thread.start_new_thread( player_info, () )
+except:
+	syslog.syslog(syslog.LOG_ERR, 'Error: unable to start player info collector thread')
+	raise
+
+# Create discovery packet broadcast thread
+try:
+	thread.start_new_thread( broadcast_discovery, (10,) )
+except:
+	syslog.syslog(syslog.LOG_ERR, 'Error: unable to start discovery broadcast thread')
+	raise
+
+syslog.syslog(syslog.LOG_NOTICE, "agocontrol jointspace device is running")
+
+while True:
+	try:
+		message = receiver.fetch(timeout=1)
+		if message.content:
+			if 'command' in message.content:
+				#print message
+				if message.content['command'] == 'discover':
+					syslog.syslog(syslog.LOG_NOTICE, "discovering devices")
+					#discovery()
+				elif message.content['command'] == 'inventory':
+					syslog.syslog(syslog.LOG_NOTICE, "ignoring inventory command")
+				else:
+					#print "ignoring "+message.content['command']+" command"
+					'''
+					if 'uuid' in message.content:
+						for (path, uuid) in uuidmap.iteritems():
+							if message.content['uuid'] == uuid:
+								# send reply
+								if message.reply_to:
+									replysender = session.sender(message.reply_to)
+									response = Message("ACK")
+									try:
+										replysender.send(response)
+									except SendError, e:
+										print "Can't send ACK: ", e
+									except NotFound, e:
+										print "Can't send ACK: ", e
+								# print path, uuid
+								command = ''
+								if message.content['command'] == 'on':
+									print "device switched on"
+									#result = set_outlet_state(path, 'on')
+									#if "on" in result:
+									#	sendStateChangedEvent(uuid, 255)
+								if message.content['command'] == 'off':
+									print "device switched off"
+									#result = set_outlet_state(path, 'off')
+									#if "off" in result:
+									#	sendStateChangedEvent(uuid, 0)
+					'''
+	except Empty, e:
+		pass
+	except KeyError, e:
+		print "key error in command evaluation", e
+	except ReceiverError, e:
+		syslog.syslog(syslog.LOG_ERR, 'Error: '+e)
+		time.sleep(1)
+
