7cf924743e8b2cdb4f20566cd6114cdfbd5953ab
Harald Klein <hari@vt100.at>
2013-02-10 12:10:35 +0000

    use agoclient lib

--- devices/rain8net/agorain8net.cpp
+++ devices/rain8net/agorain8net.cpp
@@ -20,89 +20,46 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <errno.h>
-
-#include <qpid/messaging/Connection.h>
-#include <qpid/messaging/Message.h>
-#include <qpid/messaging/Receiver.h>
-#include <qpid/messaging/Sender.h>
-#include <qpid/messaging/Session.h>
-#include <qpid/messaging/Address.h>
-
-#include <uuid/uuid.h>
 #include <stdlib.h>
 
-#include "../agozwave/CDataFile.h"
+#include "agoclient.h"
 
 #include "rain8.h"
+rain8net rain8;
+int rc;
 
 using namespace std;
-using namespace qpid::messaging;
-using namespace qpid::types;
+using namespace agocontrol;
 
-Sender sender;
-
-void reportDevices(std::string uuid, std::string id) {
-	Variant::Map content;
-	Message event;
-	try {
-		content["uuid"] = uuid;
-		content["product"] = "Rain8net valve";
-		content["manufacturer"] = "WGL";
-		content["internal-id"] = id;
-		content["devicetype"] = "valve";
-		encode(content, event);
-		event.setSubject("event.device.announce");
-		sender.send(event);
-	} catch(const std::exception& error) {
-		std::cout << error.what() << std::endl;
+std::string commandHandler(qpid::types::Variant::Map content) {
+	int valve = 0;
+	valve = atoi(content["internalid"].asString().c_str());
+	if (content["command"] == "on" ) {
+		if (rain8.zoneOn(1,valve) != 0) {
+			printf("can't switch on\n");
+			return "unknown";
+		} else {
+			printf("switched on\n");
+			return "255";
+		}
+	} else if (content["command"] == "off") {
+		if (rain8.zoneOff(1,valve) != 0) {
+			printf("can't switch off\n");
+			return "unknown";
+		} else {
+			printf("switched off\n");
+			return "0";
+		}
 	}
+	return "";
 }
 
 int main(int argc, char **argv) {
-	std::string broker;
 	std::string devicefile;
-	std::string myuuid;
 
-	rain8net rain8;
-	int rc;
 
-	Variant::Map connectionOptions;
-
-	// parse config
-	CDataFile ExistingDF("/etc/opt/agocontrol/config.ini");
-
-	t_Str szBroker  = t_Str("");
-	szBroker = ExistingDF.GetString("broker", "system");
-	if ( szBroker.size() == 0 )
-		broker="localhost:5672";
-	else		
-		broker= szBroker;
-
-	t_Str szDevice = t_Str("");
-	szDevice = ExistingDF.GetString("device", "rain8net");
-	if ( szDevice.size() == 0 )
+	// szDevice = ExistingDF.GetString("device", "rain8net");
 		devicefile="/dev/ttyS_01";
-	else		
-		devicefile= szDevice;
-
-	t_Str szUsername  = t_Str("");
-	szUsername = ExistingDF.GetString("username", "system");
-	if ( szUsername.size() == 0 )
-		connectionOptions["username"]="agocontrol";
-	else		
-		connectionOptions["username"] = szUsername;
-
-	t_Str szPassword  = t_Str("");
-	szPassword = ExistingDF.GetString("password", "system");
-	if ( szPassword.size() == 0 )
-		connectionOptions["password"]="letmein";
-	else		
-		connectionOptions["password"]=szPassword;
-
-	connectionOptions["reconnect"] = "true";
-
-	Receiver receiver;
-	Session session;
 
 	if (rain8.init(devicefile.c_str()) != 0) {
 		printf("can't open rainnet device %s\n", devicefile.c_str());
@@ -113,6 +70,13 @@ int main(int argc, char **argv) {
 		exit(1);
 	}
 	printf("connection to rain8net established\n");
+
+	AgoConnection agoConnection = AgoConnection();		
+	printf("connection established\n");
+	agoConnection.addDevice("123", "dimmer");
+	agoConnection.addDevice("124", "switch");
+	agoConnection.addHandler(commandHandler);
+
 	unsigned char status;
 	if (rain8.getStatus(1,status) == 0) {
 		printf("can't get zone status, aborting\n");
@@ -121,67 +85,7 @@ int main(int argc, char **argv) {
 		printf("Zone status: 8:%d 7:%d 6:%d 5:%d 4:%d 3:%d 2:%d 1:%d", (status & 128)?1:0, (status & 64)?1:0, (status &32)?1:0,(status&16)?1:0,(status&8)?1:0,(status&4)?1:0,(status&2)?1:0,(status&1)?1:0);
 	}
 
-	Connection connection(broker, connectionOptions);
-	try {
-		connection.open(); 
-		session = connection.createSession(); 
-		receiver = session.createReceiver("agocontrol; {create: always, node: {type: topic}}"); 
-		sender = session.createSender("agocontrol; {create: always, node: {type: topic}}"); 
-	} catch(const std::exception& error) {
-		std::cerr << error.what() << std::endl;
-		connection.close();
-		printf("could not startup\n");
-		return 1;
-	}
-	myuuid = "1234";
-	reportDevices(myuuid, string("1"));
+	agoConnection.run();
 
-	while (true) {
-		try {
-			Variant::Map content;
-			Message message = receiver.fetch(Duration::SECOND * 3);
-
-			// workaround for bug qpid-3445
-			if (message.getContent().size() < 4) {
-				throw qpid::messaging::EncodingException("message too small");
-			}
-
-			decode(message, content);
-			// std::cout << content << std::endl;
-
-			if (content["command"] == "discover") {
-				reportDevices(myuuid, string("1"));
-			}
-
-			if (content["uuid"] == myuuid) {
-				printf("received command for our uuid\n");
-				int valve = 1;
-				if (content["command"] == "on" ) {
-					if (rain8.zoneOn(1,valve) != 0) {
-						printf("can't switch on\n");
-					} else {
-						printf("switched on\n");
-					}
-				} else if (content["command"] == "off") {
-					if (rain8.zoneOff(1,valve) != 0) {
-						printf("can't switch off\n");
-					} else {
-						printf("switched off\n");
-					}
-				}
-
-				const Address& replyaddress = message.getReplyTo();
-				if (replyaddress) {
-					Sender replysender = session.createSender(replyaddress);
-					Message response("ACK");
-					replysender.send(response);
-				} 
-			}
-		} catch(const NoMessageAvailable& error) {
-		} catch(const std::exception& error) {
-			std::cerr << error.what() << std::endl;
-		}
-
-	}
 }
 
