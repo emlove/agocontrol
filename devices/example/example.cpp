10a1cd698426c2b8297a107113a743d824f1a1e4
Harald Klein <hari@vt100.at>
2013-02-10 11:12:03 +0000

    add agoclient example

---
+++ devices/example/example.cpp
@@ -0,0 +1,22 @@
+#include "agoclient.h"
+
+using namespace agocontrol;
+
+std::string commandHandler(qpid::types::Variant::Map command) {
+	if (command["command"] == "on") {
+		printf("Switch %s ON\n", command["internalid"].asString().c_str());
+		return "255";
+	} else {
+		printf("Switch %s OFF\n", command["internalid"].asString().c_str());
+		return "0";
+	}	
+}
+
+int main(int argc, char **argv) {
+	AgoConnection agoConnection = AgoConnection();		
+	printf("connection established\n");
+	agoConnection.addDevice("123", "dimmer");
+	agoConnection.addDevice("124", "switch");
+	agoConnection.addHandler(commandHandler);
+	agoConnection.run();
+}
