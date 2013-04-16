ed9aac50d808a7c8e4faecfee76f89f9e9327bfc
apagg <apagg@agocontrol.com>
2013-04-16 19:23:24 +0000

    Refs #105 handel missing config parameters

--- devices/raspiGPIO/raspiGPIO.py
+++ devices/raspiGPIO/raspiGPIO.py
@@ -14,35 +14,45 @@ import agoclient
 import threading
 import time
 import RPi.GPIO as GPIO
+import syslog
 
 client = agoclient.AgoConnection("raspiGPIO")
 
 readInputs = agoclient.getConfigOption("raspiGPIO", "inputs", "")
 readOutputs = agoclient.getConfigOption("raspiGPIO", "outputs", "")
 
-inputs = map(int, readInputs.split(','))
-outputs = map(int, readOutputs.split(','))
-
 GPIO.setmode(GPIO.BCM)
 
-for pin in inputs:
-	print pin
-	GPIO.setup(pin, GPIO.IN)
-	client.addDevice(pin, "binarysensor")
-	
-for pin in outputs:
-	print pin
-	GPIO.setup(pin, GPIO.OUT)
-	client.addDevice(pin, "switch")
+try:
+	inputs = map(int, readInputs.split(','))
+except:
+	syslog.syslog(syslog.LOG_ERR, 'no valid inputs')
+	inputs = None
+else:
+	for pin in inputs:
+		#print pin
+		GPIO.setup(pin, GPIO.IN)
+		client.addDevice(pin, "binarysensor")
+
+try:
+	outputs = map(int, readOutputs.split(','))
+except:
+	syslog.syslog(syslog.LOG_ERR, 'no valid outputs')
+	print 'no valid outputs'
+else:
+	for pin in outputs:
+		#print pin
+		GPIO.setup(pin, GPIO.OUT)
+		client.addDevice(pin, "switch")
 
 def messageHandler(internalid, content):
 	if "command" in content:
 		if content["command"] == "on":
-			print "switching on test: ", internalid
+			#print "switching on: ", internalid
 			GPIO.output(internalid, True)
 			client.emitEvent(internalid, "event.device.statechanged", "255", "")
 		if content["command"] == "off":
-			print "switching off: ", internalid
+			#print "switching off: ", internalid
 			GPIO.output(internalid, False)
 			client.emitEvent(internalid, "event.device.statechanged", "0", "")
 
@@ -62,15 +72,16 @@ class readGPIO(threading.Thread):
 			for pin in inputs:
 				input[pin] = GPIO.input(pin)
 				if (not prev_input[pin]) and input[pin]:
-					print 'sensor:', pin, '255'
+					#print 'sensor:', pin, '255'
 					client.emitEvent(pin, "event.security.sensortriggered", 255, "")
 				if prev_input[pin] and (not input[pin]):
-					print 'sensor:', pin, '0'
+					#print 'sensor:', pin, '0'
 					client.emitEvent(pin, "event.security.sensortriggered", 0, "")
 				prev_input[pin] = input[pin]
-      
-background = readGPIO()
-background.setDaemon(True)
-background.start()
+
+if inputs:      
+	background = readGPIO()
+	background.setDaemon(True)
+	background.start()
 
 client.run()
