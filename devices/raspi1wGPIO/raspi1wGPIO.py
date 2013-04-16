65b5fa9de1240a83df8e56019548cfa2fcc0867b
apagg <apagg@agocontrol.com>
2013-04-16 19:50:46 +0000

    Refs #105 send event on temp change

--- devices/raspi1wGPIO/raspi1wGPIO.py
+++ devices/raspi1wGPIO/raspi1wGPIO.py
@@ -9,7 +9,8 @@
 # /etc/opt/agocontrol/config.ini
 #
 # [raspi1wGPIO]
-# interval=600
+# interval=600 # maximum time(seconds) between reports
+# change=0.2   # temperature change between reports
 #
 
 import agoclient
@@ -17,6 +18,7 @@ import threading
 import time
 import os
 import sys
+import syslog
 
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
@@ -26,21 +28,28 @@ client = agoclient.AgoConnection("raspi1wGPIO")
 readInterval = agoclient.getConfigOption("raspi1wGPIO", "interval", "600")
 interval = int(readInterval)
 
+change = float(agoclient.getConfigOption("raspi1wGPIO", "change", "0.2"))
+
+
+
 
 try:
     readDevices = (line.rstrip('\n') for line in open('/sys/bus/w1/devices/w1_bus_master1/w1_master_slaves'))
 except IOError:
-    print 'readDevices No devices exiting'
+    syslog.syslog(syslog.LOG_ERR, 'No devices exiting')
     sys.exit()
     
 devices = []
+sensordata = {}
 
 for device in readDevices:
     if 'not found.' in device:
-        print 'for device in readDevices: No devices exiting'
+        syslog.syslog(syslog.LOG_ERR, 'No devices exiting')
         sys.exit()
     devices.append(device)
-    print 'addDevice', device
+    sensordata[(device, 'temp')] = 0
+    sensordata[(device, 'lastreporttime')] = time.time()
+    #print 'addDevice', device
     client.addDevice(device, "temperaturesensor")
 
 print devices
@@ -56,32 +65,39 @@ class read1WGPIO(threading.Thread):
                 crcok = False
                 errorcounter=0
                 while (not crcok and (errorcounter < 4)):
-                    print 'errorcounter', errorcounter
+                    #print 'errorcounter:', errorcounter
                     try:
                         slaveFile = open("/sys/bus/w1/devices/" + device + "/w1_slave") 
+                        tempText = slaveFile.read() 
+                        slaveFile.close() 
                     except IOError: 
-                        print "Error can\'t open device file", device
+                        syslog.syslog(syslog.LOG_ERR, "Error can\'t open device file", device)
                         errorcounter += 1
-                    tempText = slaveFile.read() 
-                    slaveFile.close() 
-                    print tempText
+                    #print tempText
                     firstline = tempText.split("\n")[0]
                     crcstatus = firstline.split(" ")[11]
-                    print crcstatus
+                    #print 'crcstatus:', crcstatus
                     if crcstatus == 'NO':
-                        print 'Bad CRC' 
+                        syslog.syslog(syslog.LOG_ERR, 'Bad CRC' )
                         errorcounter += 1
-                        time.sleep(3)
                     if crcstatus == 'YES':
                         secondline = tempText.split("\n")[1] 
                         temperaturedata = secondline.split(" ")[9] 
                         temperature = float(temperaturedata[2:]) 
                         temperature = temperature / 1000
-                        client.emitEvent(device, "event.environment.temperaturechanged", temperature, "degC") 
-                        print device, temperature
+                        #print device, temperature
+                        if abs(sensordata[(device, 'temp')] - temperature) > change:
+                            #print 'level change:', sensordata[(device, 'temp')]
+                            client.emitEvent(device, "event.environment.temperaturechanged", temperature, "degC") 
+                            sensordata[(device, 'temp')] = temperature
+                            sensordata[(device, 'lastreporttime')] = time.time()
+                        if time.time() > sensordata[(device, 'lastreporttime')] + interval:
+                            #print 'interval:', sensordata[(device, 'temp')]
+                            client.emitEvent(device, "event.environment.temperaturechanged", temperature, "degC") 
+                            sensordata[(device, 'temp')] = temperature
+                            sensordata[(device, 'lastreporttime')] = time.time()
                         crcok = True
-            print '---', interval, '---'
-            time.sleep(interval)
+            time.sleep(3)
             
       
 background = read1WGPIO()
