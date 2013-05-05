db15cfc5af991f186cc2af17fa35c226643324f9
Harald Klein <hari@vt100.at>
2013-05-05 10:53:34 +0000

    use agoclient.py in simulator

--- devices/agosimulator.py
+++ devices/agosimulator.py
@@ -3,175 +3,57 @@
 import random
 import sys
 import syslog
-import time
-import pickle
-import optparse
-import ConfigParser
 import socket
+import threading
+import time
 
-from qpid.messaging import *
-from qpid.util import URL
-from qpid.log import enable, DEBUG, WARN
+import agoclient
 
-config = ConfigParser.ConfigParser()
-config.read('/etc/opt/agocontrol/config.ini')
+client = agoclient.AgoConnection("simulator")
 
-try:
-	username = config.get("system","username")
-except:
-	username = "agocontrol"
+def messageHandler(internalid, content):
+	if "command" in content:
+		if content["command"] == "on":
+			print "switching on: " + internalid
+			client.emitEvent(internalid, "event.device.state", "255", "")
+		if content["command"] == "off":
+			print "switching off: " + internalid
+			client.emitEvent(internalid, "event.device.state", "0", "")
+		if content['command'] == 'setlevel':
+			if 'level' in content:
+				print "device level changed", content["level"]
+				client.emitEvent(internalid, "event.device.state", level, "")
 
-try:
-	password = config.get("system","password")
-except:
-	password = "letmein"
+client.addHandler(messageHandler)
 
-try:
-	broker = config.get("system","broker")
-except:
-	broker = "localhost"
+client.addDevice("123", "dimmer")
+client.addDevice("124", "switch")
+client.addDevice("125", "binarysensor")
+client.addDevice("126", "multilevelsensor")
 
-try:
-	debug = config.get("system","debug")
-except:
-	debug = "WARN"
-
-if debug=="DEBUG":
-	enable("qpid", DEBUG)
-else:
-	enable("qpid", WARN)
-
- 
-parser = optparse.OptionParser(usage="usage: %prog <command> [options] [ PARAMETERS ... ]",
-                               description="send automation control commands")
-parser.add_option("-b", "--broker", default=broker, help="hostname of broker (default %default)")
-parser.add_option("-u", "--username", default=username, help="specify a username")
-parser.add_option("-P", "--password", default=password, help="specify a password")
-
-opts, args = parser.parse_args()
-
-# route stderr to syslog
-class LogErr:
-        def write(self, data):
-                syslog.syslog(syslog.LOG_ERR, data)
-
-syslog.openlog(sys.argv[0], syslog.LOG_PID, syslog.LOG_DAEMON)
-# sys.stderr = LogErr()
-
-connection = Connection(opts.broker, username=opts.username, password=opts.password,  reconnect=True)
-connection.open()
-session = connection.session()
-receiver = session.receiver("agocontrol; {create: always, node: {type: topic}}")
-sender = session.sender("agocontrol; {create: always, node: {type: topic}}")
-
-def reportdevice(uuid='488a0d5b-c0d3-415c-8daa-a2a4fd3261f4', type='switch', product='aGoControl simulator device'):
-	try:
-		content = {}
-		content["devicetype"]=type
-		content["uuid"] = uuid
-		content["product"] = product
-		message = Message(content=content,subject="event.device.announce")
-		sender.send(message)
-	except SendError, e:
-		print e
-
-def sendtempevent(uuid, temp):
-        try:
-                content = {}
-                content["uuid"] = uuid
-                content["level"] = float(temp)
-                content["unit"] = "degC"
-                message = Message(content=content,subject="event.environment.temperaturechanged")
-                sender.send(message)
-        except SendError, e:
-                print e
-
-def sendSensorTriggerEvent(uuid, level):
-        try:
-                content = {}
-                content["uuid"] = uuid
-                content["level"] = level
-                message = Message(content=content,subject="event.security.sensortriggered")
-                sender.send(message)
-        except SendError, e:
-                print e
-
-def sendStateChangedEvent(uuid, level):
-        try:
-                content = {}
-                content["uuid"] = uuid
-                content["level"] = level
-                message = Message(content=content,subject="event.device.statechanged")
-                sender.send(message)
-        except SendError, e:
-                print e
-
-syslog.syslog(syslog.LOG_NOTICE, "agosimulator.py startup")
-
-devices = {}
-devices["2ab0e80d-7b48-4d17-b083-58fda121d6e3"] = "switch"
-devices["40cbb44c-5997-4f30-b1f5-d4d6c2accfad"] = "switch"
-devices["52abead4-82b0-4fd2-ac3a-c9de1cb12b9b"] = "dimmer"
-devices["69fac346-875f-4a80-b9af-c53459cc13dd"] = "multilevelsensor"
-devices["5194cdcb-8d81-4d3c-a12f-19c554a70b1f"] = "dimmerrgb"
-devices["e3a5c630-12bf-4956-8d1f-1da1dfd31c0a"] = "zwavecontroller"
-
-def discovery():
-	for (uuid, devicetype) in devices.iteritems():
-		reportdevice(uuid=uuid, type=devicetype)
-
-discovery()
-
-counter = 1
-
-while True:
-	counter = counter + 1
-	try:
-		if counter > 15:
+class testEvent(threading.Thread):
+    def __init__(self,):
+        threading.Thread.__init__(self)    
+    def run(self):
+    	level = 0
+	counter = 0
+        while (True):
+		counter = counter + 1
+		if counter > 3:
 			counter = 0
 			temp = random.randint(50,300) / 10
-			sendtempevent("69fac346-875f-4a80-b9af-c53459cc13dd", temp)		
-		message = receiver.fetch(timeout=1)
-		if message.content:
-			if 'command' in message.content:
-				print message
-				if message.content['command'] == 'discover':
-					syslog.syslog(syslog.LOG_NOTICE, "discovering devices")
-					discovery()
-				elif message.content['command'] == 'inventory':
-					syslog.syslog(syslog.LOG_NOTICE, "ignoring inventory command")
-				else:
-					if 'uuid' in message.content:
-						for (uuid, devicetype) in devices.iteritems():
-							if message.content['uuid'] == uuid:
-								# print path, uuid
-								command = ''
-								if message.content['command'] == 'on':
-									sendStateChangedEvent(uuid, 255)
-									print "device switched on"
-								if message.content['command'] == 'off':
-									sendStateChangedEvent(uuid, 0)
-									print "device switched off"
-								if message.content['command'] == 'setlevel':
-									if 'level' in message.content:
-										sendStateChangedEvent(uuid, message.content["level"])
-										print "device level changed", message.content["level"]
-								# send reply
-								if message.reply_to:
-									replysender = session.sender(message.reply_to)
-									response = Message("ACK")
-									try:
-										replysender.send(response)
-									except SendError, e:
-										print "Can't send ACK: ", e
-	except Empty, e:
-		pass
-	except KeyError, e:
-		print "key error in command evaluation", e
-	except ReceiverError, e:
-		print e
-		time.sleep(1)
+			client.emitEvent("126", "event.environment.temperaturechanged", temp, "degC");
+		client.emitEvent("125", "event.security.sensortriggered", level, "")
+		if (level == 0):
+			level = 255
+		else:
+			level = 0
+		time.sleep (5)
+      
+background = testEvent()
+background.setDaemon(True)
+background.start()
 
-
-
+syslog.syslog(syslog.LOG_NOTICE, "agosimulator.py startup")
+client.run()
 
