a60a7d257cfccf7e14c7e83fe09d59c622fa0da1
Harald Klein <hari@vt100.at>
2013-05-05 12:54:11 +0000

    use agoclient in messagesend

--- core/messagesend/messagesend.cpp
+++ core/messagesend/messagesend.cpp
@@ -6,67 +6,48 @@
 #include <stdio.h>
 #include <errno.h>
 
-#include <qpid/messaging/Session.h>
-#include <qpid/messaging/Connection.h>
-#include <qpid/messaging/Address.h>
-#include <qpid/messaging/Sender.h>
-#include <qpid/messaging/Message.h>
-
-#include <qpid/types/Variant.h>
-
 #include <cstdlib>
 #include <iostream>
 #include <sstream>
 #include <vector>
 #include <ctime>
 
+#include "agoclient.h"
+
 using namespace std;
-using namespace qpid::types;
+using namespace agocontrol;
 
 using std::stringstream;
 using std::string;
 
-    static bool nameval(const std::string& in, std::string& name, std::string& value)
-    {
-        std::string::size_type i = in.find("=");
+static bool nameval(const std::string& in, std::string& name, std::string& value) {
+	std::string::size_type i = in.find("=");
         if (i == std::string::npos) {
-            name = in;
-            return false;
+		name = in;
+		return false;
         } else {
-            name = in.substr(0, i);
-            if (i+1 < in.size()) {
-                value = in.substr(i+1);
-                return true;
-            } else {
-                return false;
-            }
+		name = in.substr(0, i);
+		if (i+1 < in.size()) {
+			value = in.substr(i+1);
+			return true;
+		} else {
+			return false;
+		}
         }
-    }
+}
 
+int main(int argc, char **argv) {
+	AgoConnection agoConnection = AgoConnection("messagesend");		
 
-int main(int argc, char **argv)
-{
-		for (int i=0;i<argc;i++) {
-			printf("%i %s\n",i,argv[i]);
+	qpid::types::Variant::Map content;
+	for (int i=1;i<argc;i++) {
+		string name, value;
+		if (nameval(string(argv[i]),name, value)) {
+			content[name]=value;
 		}
-
-		Variant::Map content;
-		std::string value;
-		value = "";
-		for (int i=1;i<argc;i++) {
-			printf("%i %s\n",i,argv[i]);
-			string name;
-			name = "uuid";
-			if (nameval(string(argv[i]),name, value)) {
-				content["uuid"]=value;
-			}
-			name = "command";
-			if (nameval(string(argv[i]),name, value)) {
-				content["command"]=value;
-			}
-		}
-	cout << content << endl;
-	exit(0);
+	}
+	cout << "Sending message: " << content << endl;
+	agoConnection.sendMessage(content);
 }
 
 
