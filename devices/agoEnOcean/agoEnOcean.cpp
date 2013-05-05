51259182abe69c632d440abc0d1599ffdf872f9d
Harald Klein <hari@vt100.at>
2013-05-05 17:48:51 +0000

    use agoclient lib for enocean

--- devices/agoEnOcean/agoEnOcean.cpp
+++ devices/agoEnOcean/agoEnOcean.cpp
@@ -17,16 +17,10 @@
 #include <pthread.h>
 #include <stdio.h>
 
-#include <qpid/messaging/Connection.h>
-#include <qpid/messaging/Message.h>
-#include <qpid/messaging/Receiver.h>
-#include <qpid/messaging/Sender.h>
-#include <qpid/messaging/Session.h>
-#include <qpid/messaging/Address.h>
+#include "agoclient.h"
 
-using namespace qpid::messaging;
-using namespace qpid::types;
 using namespace std;
+using namespace agocontrol;
 
 extern "C" {
 	#include "libEnOcean/cssl.h"
@@ -35,65 +29,24 @@ extern "C" {
 	#include "libEnOcean/TCM120.h"
 }
 
-#include "../agozwave/CDataFile.h"
+AgoConnection *agoConnection;
 
 void serialCallBack(enocean_data_structure in);
 
-Sender sender;
-Receiver receiver;
-Session session;
+std::string commandHandler(qpid::types::Variant::Map content) {
+	std::string internalid = content["internalid"].asString();
+
+	return "";
+}
 
 int main(int argc, char **argv) {
-	std::string broker;
 	std::string devicefile;
+	devicefile=getConfigOption("enocean", "device", "/dev/usbenocean");
 
-	Variant::Map connectionOptions;
+	AgoConnection _agoConnection = AgoConnection("enocean");
+	agoConnection = &_agoConnection;
 
-	// parse config
-	CDataFile ExistingDF("/etc/opt/agocontrol/config.ini");
-
-	t_Str szBroker  = t_Str("");
-	szBroker = ExistingDF.GetString("broker", "system");
-	if ( szBroker.size() == 0 )
-		broker="localhost:5672";
-	else		
-		broker= szBroker;
-
-	t_Str szDevice = t_Str("");
-	szDevice = ExistingDF.GetString("device", "enocean");
-	if ( szDevice.size() == 0 )
-		devicefile="/dev/usbenocean";
-	else		
-		devicefile= szDevice;
-
-	t_Str szUsername  = t_Str("");
-	szUsername = ExistingDF.GetString("username", "system");
-	if ( szUsername.size() == 0 )
-		connectionOptions["username"]="agocontrol";
-	else		
-		connectionOptions["username"] = szUsername;
-
-	t_Str szPassword  = t_Str("");
-	szPassword = ExistingDF.GetString("password", "system");
-	if ( szPassword.size() == 0 )
-		connectionOptions["password"]="letmein";
-	else		
-		connectionOptions["password"]=szPassword;
-
-	connectionOptions["reconnect"] = "true";
-
-	Connection connection(broker, connectionOptions);
-	try {
-		connection.open(); 
-		session = connection.createSession(); 
-		receiver = session.createReceiver("agocontrol; {create: always, node: {type: topic}}"); 
-		sender = session.createSender("agocontrol; {create: always, node: {type: topic}}"); 
-	} catch(const std::exception& error) {
-		std::cerr << error.what() << std::endl;
-		connection.close();
-		printf("could not startup\n");
-		return 1;
-	}
+	printf("connection to agocontrol established\n");
 
 	printf("initalizing EnOcean\n");
 	enocean_error_structure error = enocean_init((const char *)devicefile.c_str());
@@ -123,38 +76,8 @@ int main(int argc, char **argv) {
 
 	enocean_send(&frame);
 
-	while( true )
-	{
-
-		// Do stuff
-		try{
-			Variant::Map content;
-			Message message = receiver.fetch(Duration::SECOND * 3);
-
-			// workaround for bug qpid-3445
-			if (message.getContent().size() < 4) {
-				throw qpid::messaging::EncodingException("message too small");
-			}
-
-			decode(message, content);
-			// std::cout << content << std::endl;
-				session.acknowledge();
-		} catch(const NoMessageAvailable& error) {
-			
-		} catch(const std::exception& error) {
-			std::cerr << error.what() << std::endl;
-		}
-
-	}
-
-	try {
-		connection.close();
-	} catch(const std::exception& error) {
-		std::cerr << error.what() << std::endl;
-		connection.close();
-		return 1;
-	}
-	
+	agoConnection->addHandler(commandHandler);
+	agoConnection->run();	
 }
 
 void serialCallBack(enocean_data_structure in) {
