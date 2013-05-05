19589b66d4bf72f1c15fb226ca3bbfb8a6c910bf
Harald Klein <hari@vt100.at>
2013-05-05 19:09:04 +0000

    use agoclient lib for enigma2

--- devices/enigma2/agoenigma2.py
+++ devices/enigma2/agoenigma2.py
@@ -3,14 +3,7 @@
 import sys
 import syslog
 import time
-import pickle
-import optparse
-import ConfigParser
 import socket
-
-from qpid.messaging import *
-from qpid.util import URL
-from qpid.log import enable, DEBUG, WARN
 
 from xml.dom import minidom
 
@@ -19,42 +12,7 @@ import myavahi
 import urllib
 import urllib2
 
-config = ConfigParser.ConfigParser()
-config.read('/etc/opt/agocontrol/config.ini')
-
-try:
-	username = config.get("system","username")
-except:
-	username = "agocontrol"
-
-try:
-	password = config.get("system","password")
-except:
-	password = "letmein"
-
-try:
-	broker = config.get("system","broker")
-except:
-	broker = "localhost"
-
-try:
-	debug = config.get("system","debug")
-except:
-	debug = "WARN"
-
-if debug=="DEBUG":
-	enable("qpid", DEBUG)
-else:
-	enable("qpid", WARN)
-
- 
-parser = optparse.OptionParser(usage="usage: %prog <command> [options] [ PARAMETERS ... ]",
-                               description="send automation control commands")
-parser.add_option("-b", "--broker", default=broker, help="hostname of broker (default %default)")
-parser.add_option("-u", "--username", default=username, help="specify a username")
-parser.add_option("-P", "--password", default=password, help="specify a password")
-
-opts, args = parser.parse_args()
+import agoclient
 
 # route stderr to syslog
 class LogErr:
@@ -64,76 +22,27 @@ class LogErr:
 syslog.openlog(sys.argv[0], syslog.LOG_PID, syslog.LOG_DAEMON)
 # sys.stderr = LogErr()
 
-# read persistent uuid mapping from file
-try:
-	uuidmapfile = open("/etc/opt/agocontrol/enigma2-uuidmap.pck","r")
-	uuidmap = pickle.load(uuidmapfile)
-	uuidmapfile.close()
-except IOError, e:
-	uuidmap = {}
-
-devices = {}
-
-connection = Connection(opts.broker, username=opts.username, password=opts.password,  reconnect=True)
-connection.open()
-session = connection.session()
-receiver = session.receiver("agocontrol; {create: always, node: {type: topic}}")
-sender = session.sender("agocontrol; {create: always, node: {type: topic}}")
-
-def lookupuuid(path):
-	if path in uuidmap:
-		pass
-	else:
-		newuuid = str(uuid4())
-		uuidmap[path] = newuuid
-		try:
-			print "new uuid %s %s" % (newuuid, path)
-			# uuid is new, try to store it
-			print uuidmap
-			uuidmapfile = open("/etc/opt/agocontrol/enigma2-uuidmap.pck","w")
-			pickle.dump(uuidmap, uuidmapfile)
-			uuidmapfile.close()
-		except IOError, e:
-			pass
-	return uuidmap[path]
-
-def reportdevice(path, type='settopbox', product='Dreambox/Enigma2'):
-	try:
-		content = {}
-		content["devicetype"]=type
-		content["event"] = "announce"
-		content["uuid"] = lookupuuid(path)
-		content["internal-id"] = path
-		content["product"] = product
-		message = Message(content=content,subject="event.device.announce")
-		sender.send(message)
-	except SendError, e:
-		print e
-
-def discovery():
-	for path in devices.iterkeys():
-		reportdevice(path);
+client = agoclient.AgoConnection("enigma2")
 
 syslog.syslog(syslog.LOG_NOTICE, "agoenigma2.py startup")
-
 syslog.syslog(syslog.LOG_NOTICE, "discovering devices")
-
 
 def mycallback(name, host, port):
 	if "dm500hd" in name or "dm600pvr" in name:
 		# print "callback %s %s %s\n" % (name, host, port)
-		devices[name] = host
 		try:
 			f = urllib2.urlopen('http://%s/web/about' % str(host))
 			dom1 = minidom.parseString(f.read())
 			e2model = dom1.getElementsByTagName('e2model')[0];
 			print "Found %s on %s\n" % (e2model.childNodes[0].data,str(host))
-			reportdevice(str(name))
+			client.addDevice(str(host), "settopbox")
 		except:
-			print "can't get enigma model\n" 
+			print "ERROR: Can't determine enigma version"
 
 s = myavahi.zmqconf(mycallback)
 s.browse_services("_workstation._tcp")
+
+time.sleep(3)
 
 def getText(nodelist):
 	rc = []
@@ -241,67 +150,40 @@ def getbouquets(host):
 #print "%s = %s" % (title, description)
 #print getepgnownext('192.168.80.9',bouquets['Favourites (TV)'])
 
-while True:
-	try:
-		message = receiver.fetch(timeout=1)
-		if message.content:
-			if 'command' in message.content:
-				print message
-				if message.content['command'] == 'discover':
-					syslog.syslog(syslog.LOG_NOTICE, "discovering devices")
-					discovery()
-				if message.content['command'] == 'inventory':
-					syslog.syslog(syslog.LOG_NOTICE, "ignoring inventory command")
-				else:
-					for (name, uuid) in uuidmap.iteritems():
-						if message.content['uuid'] == uuid:
-							print name, uuid
-							host = devices[name]
-							result = 'ACK'
-							if message.content['command'] == 'on':
-								setpower(host, 4)
-							if message.content['command'] == 'off':
-								setpower(host, 5)
-							if message.content['command'] == 'mute':
-								setmute(host, True)
-							if message.content['command'] == 'unmute':
-								setmute(host, False)
-							if message.content['command'] == 'mutetoggle':
-								setvolume(host, "mute")
-							if message.content['command'] == 'vol+':
-								setvolume(host, "up")
-							if message.content['command'] == 'vol-':
-								setvolume(host, "down")
-							if message.content['command'] == 'setlevel':
-								setvolume(host, 'set%s' % message.content['level'])
-							if message.content['command'] == 'zap':
-								channel = message.content['channel']
-								services = getallservices(host)
-								for (bouquet, servicelist) in services.iteritems():
-									if channel in servicelist:
-										zap(host, servicelist[channel])
-							if message.content['command'] == 'getepg':
-								epg={}
-								for (bouquet, bref) in getbouquets(host).iteritems():
-									epg[bouquet] = getepgnownext(host,bref)
-								result = epg
-								print result
-							# send reply
-							if message.reply_to:
-								replysender = session.sender(message.reply_to)
-								response = Message(result)
-								try:
-									replysender.send(response)
-								except SendError, e:
-									print "Can't send ACK: ", e
-	except Empty, e:
-		pass
-	except KeyError, e:
-		print "key error in command evaluation", e
-	except ReceiverError, e:
-		print e
-		time.sleep(1)
+def messageHandler(internalid, content):
+	if "command" in content:
+		host = internalid
+		result = 'ACK'
+		if content['command'] == 'on':
+			setpower(host, 4)
+		if content['command'] == 'off':
+			setpower(host, 5)
+		if content['command'] == 'mute':
+			setmute(host, True)
+		if content['command'] == 'unmute':
+			setmute(host, False)
+		if content['command'] == 'mutetoggle':
+			setvolume(host, "mute")
+		if content['command'] == 'vol+':
+			setvolume(host, "up")
+		if content['command'] == 'vol-':
+			setvolume(host, "down")
+		if content['command'] == 'setlevel':
+			setvolume(host, 'set%s' % message.content['level'])
+		if content['command'] == 'zap':
+			channel = message.content['channel']
+			services = getallservices(host)
+			for (bouquet, servicelist) in services.iteritems():
+				if channel in servicelist:
+					zap(host, servicelist[channel])
+		if content['command'] == 'getepg':
+			epg={}
+			for (bouquet, bref) in getbouquets(host).iteritems():
+				epg[bouquet] = getepgnownext(host,bref)
+			result = epg
+			print result
 
+client.addHandler(messageHandler)
 
-
+client.run()
 
