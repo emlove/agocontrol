7a4e5b6637dd96afe2a56a6bd429ba38bec826ec
Christoph Jaeger <christoph@agocontrol.com>
2013-05-12 12:37:37 +0000

    fix status and energy poll - now really

--- devices/agoapc/agoapc.py
+++ devices/agoapc/agoapc.py
@@ -20,7 +20,7 @@ OIDName = "1,3,6,1,4,1,318,1,1,4,5,2,1,3"	# sPDUOutletName
 OIDLoad = "1,3,6,1,4,1,318,1,1,12,2,3,1,1,2,1"	# rPDULoadStatusLoad
 loadOID = (1,3,6,1,4,1,318,1,1,12,2,3,1,1,2,1)
 
-apchost = agoclient.getConfigOption("apc", "host", "192.168.80.3")
+apchost = agoclient.getConfigOption("apc", "host", "192.168.1.13")
 apcport = int(agoclient.getConfigOption("apc", "port", "161") ) 
 apcvoltage = int(agoclient.getConfigOption("apc", "voltage", "220") ) 
 apccommunityro =  agoclient.getConfigOption("apc", "community_readonly", "public")
@@ -34,18 +34,6 @@ class LogErr:
 syslog.openlog(sys.argv[0], syslog.LOG_PID, syslog.LOG_DAEMON)
 # sys.stderr = LogErr()
 
-
-# get outlets from apc 
-myoid = eval(str(OIDOutletCount))
-__errorIndication, __errorStatus, __errorIndex, varBinds = cmdgen.CommandGenerator().getCmd(
-	cmdgen.CommunityData('my-agent', 'public', 0),
-	cmdgen.UdpTransportTarget((apchost, apcport)),
-	myoid )
-
-outletCount = varBinds[0][1]
-
-for outlet in range(1,outletCount+1):
-	client.addDevice(outlet, "switch")
 
 
 def set_outlet_state(path, command):
@@ -126,11 +114,12 @@ def print_time( threadName, delay):
 		except:
 			time.sleep(1)
 
-# Create Energy-Level thread
-#try:
-#	thread.start_new_thread( print_time, ("Energy-Level:", 10, ) )
-#except:
-#	print "Error: unable to start thread"
+# get outlets from apc 
+myoid = eval(str(OIDOutletCount))
+__errorIndication, __errorStatus, __errorIndex, varBinds = cmdgen.CommandGenerator().getCmd(
+	cmdgen.CommunityData('my-agent', 'public', 0),
+	cmdgen.UdpTransportTarget((apchost, apcport)),
+	myoid )
 
 outletCount = varBinds[0][1]
 
@@ -146,20 +135,53 @@ for outlet in range(1,outletCount+1):
 		client.emitEvent(outlet, "event.device.statechanged", "0", "")
 		print "state off"
 
+client.addDevice(powerusage, "multilevelsensor")
+
+
+
+syslog.syslog(syslog.LOG_NOTICE, "agoapc.py startup")
+
+
+class EnergyUsage(threading.Thread):
+	def __init__(self,):
+		threading.Thread.__init__(self)
+	def run(self):
+
+		old_currentPower = 0
+		unit = "Wh"
+		while (True):
+			try:
+				time.sleep (5)	
+				currentPowerA = get_current_power()
+				currentPower = int(float(currentPowerA * apcvoltage))
+				if currentPower != old_currentPower:
+					client.emitEvent(powerusage, "event.environment.powerusage", currentPower, unit)
+					print currentPower
+					old_currentPower = currentPower
+			except:
+				time.sleep(1)
+			
+
 def messageHandler(internalid, content):
 	if "command" in content:
 		if content["command"] == "on":
 			print "device switched on: ", internalid
-			result = set_outlet_state(internalid, 'on')
+			path = internalid
+			result = set_outlet_state(path, 'on')
 			if "on" in result:
 				client.emitEvent(internalid, "event.device.statechanged", "255", "")
 		if content["command"] == "off":
 			print "device switched off:", internalid
+			path = internalid
 			result = set_outlet_state(path, 'off')
 			if "off" in result:
 				client.emitEvent(internalid, "event.device.statechanged", "0", "")
 
-client.addHandler(messageHandler)
 
+background = EnergyUsage()
+background.setDaemon(True)
+background.start()
+
+client.addHandler(messageHandler)
 client.run()
 
