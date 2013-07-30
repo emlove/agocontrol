1b9b4b2af14702e1420cd116e6ffc39ae37af914
Harald Klein <hari@vt100.at>
2013-07-30 19:50:00 +0000

    refs #124 - add new C++ proxy based onvif code

---
+++ devices/onvif/onvif.cpp
@@ -0,0 +1,218 @@
+#include <stdio.h>
+#include <string>
+#include <map>
+
+#include <uuid/uuid.h>
+
+#include "soapMediaBindingProxy.h"
+#include "soapDeviceBindingProxy.h"
+#include "soapwsddProxy.h"
+#include "onvif.nsmap"
+#include "stdsoap2.h"
+#include "soapStub.h"
+
+#include "wsseapi.h"
+#include "wsaapi.h"
+
+std::string generateUuid() {
+	std::string strUuid;
+	char *name;
+	if ((name=(char*)malloc(37)) != NULL) {
+		uuid_t tmpuuid;
+		name[0]=0;
+		name[36]=0;
+		uuid_generate(tmpuuid);
+		uuid_unparse(tmpuuid,name);
+		strUuid = std::string(name);
+		free(name);
+	}
+	return strUuid;
+}
+
+std::string getRTSPUri(std::string mediaXaddr, std::string username, std::string password, std::string profile) {
+	std::string uri;
+
+	MediaBindingProxy mediaProxy(mediaXaddr.c_str());
+
+	_trt__GetStreamUri trt__GetStreamUri;
+	_trt__GetStreamUriResponse trt__GetStreamUriResponse;
+	
+	tt__StreamSetup streamSetup;
+	tt__ReferenceToken referenceToken;
+
+	// we want a RTP unicast
+	tt__StreamType streamType = tt__StreamType__RTP_Unicast;
+	// via UDP transport
+	tt__TransportProtocol transportProtocol = tt__TransportProtocol__UDP;
+	tt__Transport transport;
+
+	transport.Protocol = transportProtocol;
+	streamSetup.Stream  = streamType;
+	streamSetup.Transport  = &transport;
+
+	trt__GetStreamUri.StreamSetup = &streamSetup;
+	trt__GetStreamUri.ProfileToken = profile.c_str();
+
+	soap_wsse_add_Security(&mediaProxy);
+	soap_wsse_add_UsernameTokenDigest(&mediaProxy, NULL, username.c_str(), password.c_str());
+
+	int result = mediaProxy.GetStreamUri(&trt__GetStreamUri, &trt__GetStreamUriResponse);
+#ifdef DEBUG
+	printf("SOAP Result: %d\n", result);
+#endif
+	if (result == SOAP_OK) {
+#ifdef DEBUG
+		printf("Stream: %s\n", trt__GetStreamUriResponse.MediaUri->Uri.c_str());
+#endif
+		uri = trt__GetStreamUriResponse.MediaUri->Uri;
+	} else {
+		printf("ERROR: GetStreamUri: %s\n", mediaProxy.soap_fault_detail());
+		uri = "";
+	}
+	mediaProxy.destroy();
+	return uri;
+}
+
+std::map <std::string, std::string> getProfiles(std::string mediaXaddr, std::string username, std::string password) {
+	std::map<std::string, std::string> profiles;
+
+	MediaBindingProxy mediaProxy(mediaXaddr.c_str());
+
+	_trt__GetProfiles trt__GetProfiles;
+	_trt__GetProfilesResponse trt__GetProfilesResponse;
+
+	soap_wsse_add_Security(&mediaProxy);
+	soap_wsse_add_UsernameTokenDigest(&mediaProxy, NULL, username.c_str(), password.c_str());
+
+	int result = mediaProxy.GetProfiles (&trt__GetProfiles, &trt__GetProfilesResponse);
+#ifdef DEBUG
+        printf("SOAP Result: %d\n", result);
+#endif
+	if (result == SOAP_OK) {
+		for(std::vector<tt__Profile * >::const_iterator it = trt__GetProfilesResponse.Profiles.begin(); it != trt__GetProfilesResponse.Profiles.end(); ++it) {
+			tt__Profile* profile = *it;
+			profiles[profile->token]=profile->Name;
+#ifdef DEBUG
+			printf("Profile: %s: %s\n", profile->token.c_str(), profile->Name.c_str());
+#endif
+		}
+	} else {
+		printf("ERROR: GetProfiles: %s\n", mediaProxy.soap_fault_detail());
+	}
+	mediaProxy.destroy();
+	return profiles;
+}
+
+int getCapabilities(std::string deviceXaddr, std::string username, std::string password, _tds__GetCapabilitiesResponse &response) {
+        DeviceBindingProxy deviceProxy(deviceXaddr.c_str());
+
+	_tds__GetCapabilities tds__GetCapabilities;
+
+	soap_wsse_add_Security(&deviceProxy);
+	soap_wsse_add_UsernameTokenDigest(&deviceProxy, NULL, username.c_str(), password.c_str());
+
+	int result =deviceProxy.GetCapabilities(&tds__GetCapabilities, &response);
+#ifdef DEBUG
+        printf("SOAP Result: %d\n", result);
+#endif
+	/* if (result == SOAP_OK) {
+		printf("Media Service: %s\n",tds__GetCapabilitiesResponse.Capabilities->Media->XAddr.c_str());
+	} else {
+		printf("ERROR: GetCapabilities: %s\n", deviceProxy.soap_fault_detail());
+	} */
+	deviceProxy.destroy();
+	return result;
+}
+
+void getDeviceInformation(std::string deviceXaddr, std::string username, std::string password) {
+        DeviceBindingProxy deviceProxy(deviceXaddr.c_str());
+
+	_tds__GetDeviceInformation tds__GetDeviceInformation;
+	_tds__GetDeviceInformationResponse tds__GetDeviceInformationResponse;
+
+	soap_wsse_add_Security(&deviceProxy);
+	soap_wsse_add_UsernameTokenDigest(&deviceProxy, NULL, username.c_str(), password.c_str());
+
+	int result = deviceProxy.GetDeviceInformation(&tds__GetDeviceInformation, &tds__GetDeviceInformationResponse);
+#ifdef DEBUG
+        printf("SOAP Result: %d\n", result);
+#endif
+	if (result == SOAP_OK) {
+			printf("Manufacturer: %s\n",tds__GetDeviceInformationResponse.Manufacturer.c_str());
+			printf("Model: %s\n",tds__GetDeviceInformationResponse.Model.c_str());
+			printf("FirmwareVersion: %s\n",tds__GetDeviceInformationResponse.FirmwareVersion.c_str());
+			printf("Serial Number: %s\n",tds__GetDeviceInformationResponse.SerialNumber.c_str());
+			printf("HardwareId: %s\n",tds__GetDeviceInformationResponse.HardwareId.c_str());
+	} else {
+		printf("ERROR: GetDeviceInformation: %s\n", deviceProxy.soap_fault_detail());
+	}
+	deviceProxy.destroy();
+}
+
+int main (int argc, char ** argv)  
+{  
+
+
+/*
+	struct wsdd__ProbeType probeType;
+	struct __wsdd__ProbeMatches matches;
+	struct wsdd__ScopesType scopesType;
+	struct SOAP_ENV__Header header;
+
+	scopesType.__item = (char *)"onvif://www.onvif.org";
+	probeType.Scopes = &scopesType;
+
+	probeType.Types = (char *)"tdn:NetworkVideoTransmitter";
+*/
+
+	struct wsdd__ProbeType probe;
+	struct __wsdd__ProbeMatches matches;
+	probe.Scopes = new struct wsdd__ScopesType();
+	probe.Types = (char*)"tdn:NetworkVideoTransmitter";
+
+	std::string tmpuuid = "urn:uuid:" +  generateUuid();
+	printf("tmpuuid: %s\n", tmpuuid.c_str());
+
+	wsddProxy *discoverProxy = new wsddProxy("soap.udp://239.255.255.250:3702/");
+	discoverProxy->soap_header((char*)tmpuuid.c_str(), NULL, NULL, NULL, NULL, "urn:schemas-xmlsoap-org:ws:2005:04:discovery", "http://schemas.xmlsoap.org/ws/2005/04/discovery/Probe", NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+	discoverProxy->recv_timeout=5;
+
+	printf("Sending probe\n");
+	discoverProxy->send_Probe(&probe);
+	printf("waiting for matches\n");
+	std::map<std::string, std::string> networkvideotransmitters;
+	while ( discoverProxy->recv_ProbeMatches(matches) == SOAP_OK) {
+		printf("waiting for matches\n");
+//		printf("match size: %d\n",matches.wsdd__ProbeMatches->__sizeProbeMatch);
+//		printf("Endpoint Addr: %s\n",matches.wsdd__ProbeMatches->ProbeMatch->wsa__EndpointReference.Address);
+		printf("Service Addr: %s\n", matches.wsdd__ProbeMatches->ProbeMatch->XAddrs);
+		printf("Type: %s\n", matches.wsdd__ProbeMatches->ProbeMatch->Types);
+		printf("Metadata Ver: %d\n",matches.wsdd__ProbeMatches->ProbeMatch->MetadataVersion);
+		printf("Scopes Addr: %s\n", matches.wsdd__ProbeMatches->ProbeMatch->Scopes->__item);
+		networkvideotransmitters[matches.wsdd__ProbeMatches->ProbeMatch->XAddrs] = matches.wsdd__ProbeMatches->ProbeMatch->Scopes->__item;
+
+	}
+	discoverProxy->destroy();
+	for (std::map<std::string, std::string>::const_iterator it = networkvideotransmitters.begin(); it != networkvideotransmitters.end(); ++it) {
+		printf("Found: %s\n", it->first.c_str());
+
+		std::string deviceService = it->first;
+		std::string m_username = "onvif";
+		std::string m_password = "onvif";
+		std::string mediaService;
+
+		getDeviceInformation(deviceService, m_username, m_password);
+		_tds__GetCapabilitiesResponse response;
+		if ( getCapabilities(deviceService, m_username, m_password, response) == SOAP_OK) {
+			mediaService= response.Capabilities->Media->XAddr.c_str(); // segfaults on direct std::string = std::string assignment??
+			//printf("Mediaservice: %s\n",mediaService.c_str());
+
+			std::map <std::string, std::string> profiles;
+			profiles = getProfiles(mediaService, m_username, m_password);
+			std::map <std::string, std::string>::const_iterator it = profiles.find("balanced_h264");
+			if (it != profiles.end()) { // cam supports balanced_h264 profile, get the URI
+				printf("URI: %s\n", getRTSPUri(mediaService, m_username, m_password, "balanced_h264").c_str());
+			}
+		}
+	}
+} 
