2717893653181f0fd0ecd7dd7458d77857b93185
Harald Klein <hari@vt100.at>
2013-07-31 17:27:26 +0000

    refs #127 - try to make u1204 happy - overwrite LDFLAGS

--- devices/mediaproxy/agomediaproxy.cpp
+++ devices/mediaproxy/agomediaproxy.cpp
@@ -8,13 +8,6 @@
 #include "BasicUsageEnvironment.hh"
 #include "agoclient.h"
 
-#include <qpid/messaging/Connection.h>
-#include <qpid/messaging/Message.h>
-#include <qpid/messaging/Receiver.h>
-#include <qpid/messaging/Sender.h>
-#include <qpid/messaging/Session.h>
-#include <qpid/messaging/Address.h>
-
 using namespace std;
 using namespace agocontrol;
 
