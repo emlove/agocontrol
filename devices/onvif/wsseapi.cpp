39f0bc5b9f50f92d8dac633deceb6800916709d4
Harald Klein <hari@vt100.at>
2013-08-02 14:18:59 +0000

    refs #124 - add time offset to wsse token digest authentication

--- devices/onvif/wsseapi.cpp
+++ devices/onvif/wsseapi.cpp
@@ -1583,6 +1583,7 @@ static int soap_wsse_session_verify(struct soap *soap, const char hash[SOAP_SMD_
 static void soap_wsse_session_cleanup(struct soap *soap);
 static void calc_digest(struct soap *soap, const char *created, const char *nonce, int noncelen, const char *password, char hash[SOAP_SMD_SHA1_SIZE]);
 static void calc_nonce(struct soap *soap, char nonce[SOAP_WSSE_NONCELEN]);
+static void calc_nonce_offset(struct soap *soap, char nonce[SOAP_WSSE_NONCELEN], long offset);
 
 static int soap_wsse_init(struct soap *soap, struct soap_wsse_data *data, const void *(*arg)(struct soap*, int*, const char*, int*));
 static int soap_wsse_copy(struct soap *soap, struct soap_plugin *dst, struct soap_plugin *src);
@@ -1857,14 +1858,33 @@ Note: this release supports the use of at most one UsernameToken in the header.
 */
 int
 soap_wsse_add_UsernameTokenDigest(struct soap *soap, const char *id, const char *username, const char *password)
+{ return soap_wsse_add_UsernameTokenDigestOffset(soap, id, username, password, 0);
+} 
+
+/**
+@fn int soap_wsse_add_UsernameTokenDigestOffset(struct soap *soap, const char *id, const char *username, const char *password, long offset)
+@brief Adds UsernameToken element for digest authentication.
+@param soap context
+@param[in] id string for signature referencing or NULL
+@param[in] username string
+@param[in] password string
+@param[in] time offset from current time in seconds
+@return SOAP_OK
+
+Computes SHA1 digest of the time stamp with offset, a nonce, and the password. The digest
+provides the authentication credentials. Passwords are NOT sent in the clear.
+Note: this release supports the use of at most one UsernameToken in the header.
+*/
+int
+soap_wsse_add_UsernameTokenDigestOffset(struct soap *soap, const char *id, const char *username, const char *password, long offset)
 { _wsse__Security *security = soap_wsse_add_Security(soap);
-  time_t now = time(NULL);
+  time_t now = time(NULL) + offset;
   const char *created = soap_dateTime2s(soap, now);
   char HA[SOAP_SMD_SHA1_SIZE], HABase64[29];
   char nonce[SOAP_WSSE_NONCELEN], *nonceBase64;
   DBGFUN2("soap_wsse_add_UsernameTokenDigest", "id=%s", id?id:"", "username=%s", username?username:"");
   /* generate a nonce */
-  calc_nonce(soap, nonce);
+  calc_nonce_offset(soap, nonce, offset);
   nonceBase64 = soap_s2base64(soap, (unsigned char*)nonce, NULL, SOAP_WSSE_NONCELEN);
   /* The specs are not clear: compute digest over binary nonce or base64 nonce? */
   /* compute SHA1(created, nonce, password) */
@@ -3873,8 +3893,26 @@ calc_digest(struct soap *soap, const char *created, const char *nonce, int nonce
 */
 static void
 calc_nonce(struct soap *soap, char nonce[SOAP_WSSE_NONCELEN])
+{ calc_nonce_offset(soap, nonce, 0);
+}
+
+/******************************************************************************\
+ *
+ * Calculate randomized hex nonce with time offset
+ *
+\******************************************************************************/
+
+/**
+@fn static void calc_nonce(struct soap *soap, char nonce[SOAP_WSSE_NONCELEN], long offset)
+@brief Calculates randomized nonce (also uses time() + offset in case a poorly seeded PRNG is used)
+@param soap context
+@param offset offset from current time in seconds
+@param[out] nonce value
+*/
+static void
+calc_nonce_offset(struct soap *soap, char nonce[SOAP_WSSE_NONCELEN], long offset)
 { int i;
-  time_t r = time(NULL);
+  time_t r = time(NULL) + offset;
   memcpy(nonce, &r, 4);
   for (i = 4; i < SOAP_WSSE_NONCELEN; i += 4)
   { r = soap_random;
