a9d273dfe5ed63e1dd3c5a9cca64ce600a1f17a7
Harald Klein <hari@vt100.at>
2013-08-23 16:59:53 +0000

    execute scenario as background thread

--- core/scenario/agoscenario.cpp
+++ core/scenario/agoscenario.cpp
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <pthread.h>
+
 #include <string>
 #include <iostream>
 #include <sstream>
@@ -23,39 +25,57 @@ bool compare(int a, int b) {
 	return a < b ? false : true;
 }
 
+void *runscenario(void * _scenario) {
+	qpid::types::Variant::Map *scenariop = (qpid::types::Variant::Map *) _scenario;
+	qpid::types::Variant::Map scenario = *scenariop;
+	// build sorted list of scenario elements
+	std::list<int> elements;
+	for (qpid::types::Variant::Map::const_iterator it = scenario.begin(); it!= scenario.end(); it++) {
+		// cout << it->first << endl;
+		// cout << it->second << endl;
+		elements.push_back(atoi(it->first.c_str()));
+	}
+	// cout << "elements: " << elements << endl;
+	elements.sort();
+	for (std::list<int>::const_iterator it = elements.begin(); it != elements.end(); it++) {
+		// cout << *it << endl;
+		int seq = *it;
+		stringstream sseq;
+		sseq << seq;
+		qpid::types::Variant::Map element = scenario[sseq.str()].asMap();
+		cout << sseq.str() << ":" << scenario[sseq.str()] << endl;
+		if (scenario["command"] == "scenariosleep") {
+			int delay = scenario["delay"];
+			sleep(delay);
+		} else { 
+			agoConnection->sendMessage(element);
+		}
+	}
+
+	return NULL;
+}
+
 std::string commandHandler(qpid::types::Variant::Map content) {
 	std::string internalid = content["internalid"].asString();
 	if (internalid == "scenariocontroller") {
+		if (content["command"] == "setscenario") {
 
+
+		} else if (content["command"] == "getscenario") {
+
+
+                } else if (content["command"] == "delscenario") {
+
+
+                } 
 
 	} else {
 
 		if (content["command"] == "on" ) {
-			cout << "executing scenario: " << internalid << endl;
-			// build sorted list of scenario elements
-			qpid::types::Variant::Map scenario = scenariomap[internalid].asMap();
-			std::list<int> elements;
-			for (qpid::types::Variant::Map::const_iterator it = scenario.begin(); it!= scenario.end(); it++) {
-				// cout << it->first << endl;
-				// cout << it->second << endl;
-				elements.push_back(atoi(it->first.c_str()));
-			}
-			// cout << "elements: " << elements << endl;
-			elements.sort();
-			for (std::list<int>::const_iterator it = elements.begin(); it != elements.end(); it++) {
-				// cout << *it << endl;
-				int seq = *it;
-				stringstream sseq;
-				sseq << seq;
-				qpid::types::Variant::Map element = scenario[sseq.str()].asMap();
-				cout << sseq.str() << ":" << scenario[sseq.str()] << endl;
-				if (content["command"] == "scenariosleep") {
-					int delay = content["delay"];
-					sleep(delay);
-				} else { 
-					agoConnection->sendMessage(element);
-				}
-			}
+			cout << "spawning thread for scenario: " << internalid << endl;
+			// runscenario((void *)&scenario);
+			pthread_t execThread;
+			pthread_create(&execThread, NULL, runscenario, (void *)&scenariomap[internalid].asMap());
 		} 
 
 	}
