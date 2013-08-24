2d2767c4d98afb84ca79933ca68561502644ac65
Christoph Jaeger <christoph@agocontrol.com>
2013-08-24 01:04:46 +0000

    try editable list - some bug inside

--- core/rpc/html/js/app.config.rooms.js
+++ core/rpc/html/js/app.config.rooms.js
@@ -4,10 +4,31 @@
  * @returns {roomConfig}
  */
 function roomConfig() {
-    this.rooms = ko.observableArray([]);
     this.hasNavigation = ko.observable(true);
+    this.rooms = ko.observableArray([]);
+
     /* get uuid into rooms */
     for (var uuid in rooms) { var tmp = rooms[uuid]; tmp.uuid = uuid; this.rooms.push(tmp); }
+
+    this.makeEditableRooms = function() {
+        var eTable = $("#configRoomsTable").dataTable();
+        eTable.fnDestroy();
+        eTable = $("#configRoomsTable").dataTable();
+        eTable.$('td.edit_room').editable(function(value, settings) {
+            var content = {};
+            content.uuid = $(this).data('uuid');
+            content.command = "setroomname";
+            content.name = value;
+            sendCommand(content);
+            return value;
+        }, {
+            data : function(value, settings) {
+                return value;
+            },
+            onblur : "cancel"
+        });
+
+    };
 }
 
 /**
