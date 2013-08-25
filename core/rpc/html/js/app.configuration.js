dc0be4ca320b49a14ecc1d2986b2bdf40be3b1ec
Christoph Jaeger <christoph@agocontrol.com>
2013-08-25 13:27:14 +0000

    variable tests

--- core/rpc/html/js/app.configuration.js
+++ core/rpc/html/js/app.configuration.js
@@ -3,20 +3,18 @@
  * @returns {configuration}
  */
 function configuration() {
-    this.systemvar = ko.observableArray([]);
     this.hasNavigation = ko.observable(true);
-
-    var systemvar = {
-        uuid: '164a5b03-d7d2-43de-94ea-fe2d7ac4617d',
-        personAge: 123
-    };
+    this.systemvar = ko.observableArray([]);
+    var tmp = {};
+    tmp.uuid = systemvar[uuid];
+    this.systemvar.push(tmp);
+    console.debug(this.systemvar);
 }
 
 /**
  * Initalizes the Configuration model
  */
 function init_configuration() {
-    console.debug(systemvar);
     model = new configuration();
 
     model.mainTemplate = function() {
