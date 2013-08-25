966bb7ecb37ef5dbb3e9c5bc21ccdba9e41e0a6a
Harald Klein <hari@vt100.at>
2013-08-25 11:21:29 +0000

    conversion script for the old agoscenario.py pickle file to json

---
+++ scripts/convert-scenario.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+import simplejson
+import sys
+import os
+import pickle
+
+scenariomap = {}
+
+# read persistent uuid mapping from file
+try:
+	scenariomapfile = open("/etc/opt/agocontrol/scenarios.pck","r")
+	scenariomap = pickle.load(scenariomapfile)
+	scenariomapfile.close()
+except IOError, e:
+	print "error"
+
+with open('/etc/opt/agocontrol/scenariomap.json' , 'w') as outfile:
+	simplejson.dump(scenariomap, outfile)
+outfile.close()
