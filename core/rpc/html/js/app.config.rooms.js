9582ca0ec062e6b5f0ec3ca6df31a21622bafadf
Adel <adel@agocontrol.com>
2013-09-02 11:33:32 +0000

    Fix rooms table and add loading message for long ops

--- core/rpc/html/js/app.config.rooms.js
+++ core/rpc/html/js/app.config.rooms.js
@@ -7,43 +7,69 @@ function roomConfig() {
     this.hasNavigation = ko.observable(true);
     this.rooms = ko.observableArray([]);
 
+    var self = this;
+
     this.makeEditable = function() {
-        var eTable = $("#configRoomsTable").dataTable();
-        eTable.fnDestroy();
-        eTable = $("#configRoomsTable").dataTable();
-        eTable.$('td.edit_room').editable(function(value, settings) {
-            var content = {};
-            content.uuid = $(this).data('uuid');
-            content.command = "setroomname";
-            content.name = value;
-            sendCommand(content);
-            return value;
-        }, {
-            data : function(value, settings) {
-                return value;
-            },
-            onblur : "cancel"
-        });
+	var eTable = $("#configRoomsTable").dataTable();
+	eTable.fnDestroy();
+	eTable = $("#configRoomsTable").dataTable();
+	eTable.$('td.edit_room').editable(function(value, settings) {
+	    var content = {};
+	    content.uuid = $(this).data('uuid');
+	    content.command = "setroomname";
+	    content.name = value;
+	    sendCommand(content);
+	    return value;
+	}, {
+	    data : function(value, settings) {
+		return value;
+	    },
+	    onblur : "cancel"
+	});
     };
 
     this.createRoom = function(data, event) {
-        var content = {};
-        content.name = $("#roomName").val();
-        content.command = 'setroomname';
-        sendCommand(content, function() {
-            unsubscribe();
-            document.location.reload();
-        });
+	$('#configRoomsTable').block({ 
+            message: '<div>Please wait ...</div>', 
+            css: { border: '3px solid #a00' } 
+        }); 
+	var content = {};
+	content.name = $("#roomName").val();
+	content.command = 'setroomname';
+	sendCommand(content, function(res) {
+	    if (res.result && res.result.returncode == 0) {
+		self.rooms.push({
+		    uuid : res.result.uuid,
+		    name : content.name,
+		    location : ""
+		});
+	    } else {
+		alert("Error while creating room!");
+	    }
+	    $('#configRoomsTable').unblock();
+	});
     };
 
-    this.deleteRoom = function(item) {
-        var content = {};
-        content.uuid = item.uuid;
-        content.command = 'deleteroom';
-        sendCommand(content, function() {
-            unsubscribe();
-            document.location.reload();
+    this.deleteRoom = function(item, event) {
+	$('#configRoomsTable').block({ 
+            message: '<div>Please wait ...</div>', 
+            css: { border: '3px solid #a00' } 
         }); 
+	var content = {};
+	content.uuid = item.uuid;
+	content.command = 'deleteroom';
+	sendCommand(content, function(res) {
+	    if (res.result && res.result.returncode == 0) {
+		self.rooms.remove(function(e) {
+		    return e.uuid == item.uuid;
+		});
+		$("#configRoomsTable").dataTable().fnDeleteRow(event.target.parentNode.parentNode);
+		$("#configRoomsTable").dataTable().fnDraw();
+	    } else {
+		alert("Error while deleting room!");
+	    }
+	    $('#configRoomsTable').unblock();
+	});
     };
 
 }
