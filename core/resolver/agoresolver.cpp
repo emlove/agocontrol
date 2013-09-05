dd00742aec791bd4775af0cefc4723eca0a0f529
Harald Klein <hari@vt100.at>
2013-09-05 13:34:39 +0000

    use agoclient in resolver

--- core/resolver/agoresolver.cpp
+++ core/resolver/agoresolver.cpp
@@ -47,48 +47,19 @@ using namespace qpid::messaging;
 using namespace qpid::types;
 using namespace agocontrol;
 
+AgoConnection *agoConnection;
 
-// qpid session and sender/receiver
-Receiver receiver;
-Sender sender;
-Session session;
-Connection agoConnection;
+Variant::Map inventory; // used to hold device registrations
+Variant::Map schema;  
+Variant::Map system; // holds system information
 
-bool replyMessage(const Address& replyaddress, Message response) {
-	if (replyaddress) {
-		try {
-			Session replysession = agoConnection.createSession();
-			Sender replysender = session.createSender(replyaddress);
-			replysender.send(response);
-			replysession.close();
-			return true;
-		} catch(const std::exception& error) {
-			clog << agocontrol::kLogErr << "cannot reply to request: " << error.what() << std::endl;
-		}
-	}
-	return false;
-}
-
-bool sendMessage(const char *subject, qpid::types::Variant::Map content) {
-        Message message;
-
-        try {
-                encode(content, message);
-                message.setSubject(subject);
-                sender.send(message);
-        } catch(const std::exception& error) {
-                std::cerr << error.what() << std::endl;
-                return false;
-        }
-
-        return true;
-}
+Inventory *inv;
 
 bool emitNameEvent(const char *uuid, const char *eventType, const char *name) {
         Variant::Map content;
         content["name"] = name;
         content["uuid"] = uuid;
-        return sendMessage(eventType, content);
+        return agoConnection->sendMessage(eventType, content);
 }
 
 bool emitFloorplanEvent(const char *uuid, const char *eventType, const char *floorplan, int x, int y) {
@@ -97,239 +68,7 @@ bool emitFloorplanEvent(const char *uuid, const char *eventType, const char *flo
         content["floorplan"] = floorplan;
         content["x"] = x;
         content["y"] = y;
-        return sendMessage(eventType, content);
-}
-void handleEvent(Variant::Map *device, string subject, Variant::Map *content);
-
-int main(int argc, char **argv) {
-	string broker;
-	string port; 
-	string schemafile;
-	Variant::Map connectionOptions;
-
-	clog.rdbuf(new agocontrol::Log("agoresolver", LOG_LOCAL0));
-	clog << agocontrol::kLogNotice << "starting up" << std::endl;
-
-	broker=getConfigOption("system", "broker", "localhost:5672");
-	connectionOptions["username"]=getConfigOption("system", "username", "agocontrol");
-	connectionOptions["password"]=getConfigOption("system", "password", "letmein");
-	schemafile=getConfigOption("system", "schema", "/etc/opt/agocontrol/schema.yaml");
-
-	connectionOptions["reconnect"] = "true";
-
-	clog << agocontrol::kLogDebug << "connecting to broker" << std::endl;
-	agoConnection = Connection(broker, connectionOptions);
-	try {
-		agoConnection.open(); 
-		session = agoConnection.createSession(); 
-		receiver = session.createReceiver("agocontrol; {create: always, node: {type: topic}}"); 
-		sender = session.createSender("agocontrol; {create: always, node: {type: topic}}"); 
-	} catch(const std::exception& error) {
-		std::cerr << error.what() << std::endl;
-		agoConnection.close();
-		printf("could not startup\n");
-		return 1;
-	}
-
-
-	Variant::Map inventory; // used to hold device registrations
-	Variant::Map schema;  
-	Variant::Map system; // holds system information
-	system["uuid"] = getConfigOption("system", "uuid", "00000000-0000-0000-000000000000");
-	system["version"] = AGOCONTROL_VERSION;
-
-	clog << agocontrol::kLogDebug << "parsing schema file" << std::endl;
-	schema = parseSchema(schemafile.c_str());
-
-	clog << agocontrol::kLogDebug << "reading inventory" << std::endl;
-	Inventory inv("/etc/opt/agocontrol/inventory.db");
-	
-	// discover devices
-	clog << agocontrol::kLogDebug << "discovering devices" << std::endl;
-	Variant::Map discovercmd;
-	discovercmd["command"] = "discover";
-	Message discovermsg;
-	try {
-		encode(discovercmd, discovermsg);
-		sender.send(discovermsg);
-	} catch(const std::exception& error) {
-		clog << agocontrol::kLogEmerg << "can't discover devices" << std::endl;
-		return 1;
-	}
-
-	while (true) {
-		try{
-			Variant::Map content;
-			string subject;
-			Message message = receiver.fetch(Duration::SECOND * 3);
-			// clog << agocontrol::kLogDebug << "acknowledge message" << std::endl;
-			session.acknowledge();
-
-			// workaround for bug qpid-3445
-			if (message.getContent().size() < 4) {
-				clog << agocontrol::kLogDebug << "working aroung qpid bug 3445" << std::endl;
-				throw qpid::messaging::EncodingException("message too small");
-			}
-
-			// clog << agocontrol::kLogDebug << "decoding message" << std::endl;
-			decode(message, content);
-			subject = message.getSubject();
-			// clog << agocontrol::kLogDebug << "subject:" << subject << "size:" << subject.size() << std::endl;
-
-			// test if it is an event
-			if (subject.size()>0) {
-				if (subject == "event.device.announce") {
-					string uuid = content["uuid"];
-					if (uuid != "") {
-						// clog << agocontrol::kLogDebug << "preparing device: uuid="  << uuid << std::endl;
-						Variant::Map device;
-						Variant::Map values;
-						device["devicetype"]=content["devicetype"].asString();
-						device["internalid"]=content["internalid"].asString();
-						device["handled-by"]=content["handled-by"].asString();
-						// clog << agocontrol::kLogDebug << "getting name from inventory" << endl;
-						device["name"]=inv.getdevicename(content["uuid"].asString());
-						device["name"].setEncoding("utf8");
-						// clog << agocontrol::kLogDebug << "getting room from inventory" << endl;
-						device["room"]=inv.getdeviceroom(content["uuid"].asString()); 
-						device["room"].setEncoding("utf8");
-						device["state"]="0";
-						device["state"].setEncoding("utf8");
-						device["values"]=values;
-						clog << agocontrol::kLogDebug << "adding device: uuid="  << uuid  << " type: " << device["devicetype"].asString() << std::endl;
-						inventory[uuid] = device;
-					}
-				} else if (subject == "event.device.remove") {
-					string uuid = content["uuid"];
-					if (uuid != "") {
-						clog << agocontrol::kLogDebug << "removing device: uuid="  << uuid  << std::endl;
-						Variant::Map::iterator it = inventory.find(uuid);
-						if (it != inventory.end()) {
-							inventory.erase(it);
-						}
-					}
-				} else {
-					if (content["uuid"].asString() != "") {
-						string uuid = content["uuid"];
-						// see if we have that device in the inventory already, if yes handle the event
-						if (inventory.find(uuid) != inventory.end()) handleEvent(&inventory[uuid].asMap(), subject, &content);
-					}
-
-				}
-				//printf("received event: %s\n", subject.c_str());	
-
-			} else {
-				bool isHandled = false;
-				// this is a command
-				Variant::Map reply;
-				if (content["command"] == "inventory") {
-					clog << agocontrol::kLogDebug << "responding to inventory request" << std::endl;
-					reply["inventory"] = inventory;
-					reply["schema"] = schema;	
-					reply["rooms"] = inv.getrooms();
-					reply["floorplans"] = inv.getfloorplans();
-					reply["system"] = system;
-					reply["returncode"] = 0;
-					isHandled = true;
-
-					// cout << agocontrol::kLogDebug << "inv: " << inventory << std::endl;
-				} else if (content["command"] == "setroomname") {
-					string uuid = content["uuid"];
-					// if no uuid is provided, we need to generate one for a new room
-					if (uuid == "") uuid = generateUuid();
-					inv.setroomname(uuid, content["name"]);
-					reply["uuid"] = uuid;
-					reply["returncode"] = 0;
-					isHandled = true;
-					emitNameEvent(uuid.c_str(), "event.system.roomnamechanged", content["name"].asString().c_str());
-				} else if (content["command"] == "setdeviceroom") {
-					if ((content["uuid"].asString() != "") && (inv.setdeviceroom(content["uuid"], content["room"]) == 0)) {
-						reply["returncode"] = 0;
-						// update room in local device map
-						Variant::Map *device;
-						string room = inv.getdeviceroom(content["uuid"]);
-						string uuid = content["uuid"];
-						device = &inventory[uuid].asMap();
-						(*device)["room"]= room;
-					} else {
-						reply["returncode"] = -1;
-					}
-					isHandled = true;
-				} else if (content["command"] == "setdevicename") {
-					if ((content["uuid"].asString() != "") && (inv.setdevicename(content["uuid"], content["name"]) == 0)) {
-						reply["returncode"] = 0;
-                                                // update name in local device map
-                                                Variant::Map *device;
-                                                string name = inv.getdevicename(content["uuid"]);
-                                                string uuid = content["uuid"];
-                                                device = &inventory[uuid].asMap();
-                                                (*device)["name"]= name;
-						emitNameEvent(content["uuid"].asString().c_str(), "event.system.devicenamechanged", content["name"].asString().c_str());
-                                        } else {
-						reply["returncode"] = -1;
-                                        }
-					isHandled = true;
-
-				} else if (content["command"] == "deleteroom") {
-					if (inv.deleteroom(content["uuid"]) == 0) {
-                                                string uuid = content["uuid"].asString();
-						emitNameEvent(uuid.c_str(), "event.system.roomdeleted", "");
-						reply["returncode"] = 0;
-					} else {
-						reply["returncode"] = -1;
-					}
-					isHandled = true;
-				} else if (content["command"] == "setfloorplanname") {
-					string uuid = content["uuid"];
-					// if no uuid is provided, we need to generate one for a new floorplan
-					if (uuid == "") uuid = generateUuid();
-					if (inv.setfloorplanname(uuid, content["name"]) == 0) {
-						reply["uuid"] = uuid;
-						reply["returncode"] = 0;
-						emitNameEvent(content["uuid"].asString().c_str(), "event.system.floorplannamechanged", content["name"].asString().c_str());
-					} else {
-						reply["returncode"] = -1;
-					}
-					isHandled = true;
-				} else if (content["command"] == "setdevicefloorplan") {
-					if ((content["uuid"].asString() != "") && (inv.setdevicefloorplan(content["uuid"], content["floorplan"], content["x"], content["y"]) == 0)) {
-						reply["returncode"] = 0;
-						emitFloorplanEvent(content["uuid"].asString().c_str(), "event.system.floorplandevicechanged", content["floorplan"].asString().c_str(), content["x"], content["y"]);
-					} else {
-						reply["returncode"] = -1;
-					}
-					isHandled = true;
-
-				} else if (content["command"] == "deletefloorplan") {
-					if (inv.deletefloorplan(content["uuid"]) == 0) {
-						reply["returncode"] = 0;
-						emitNameEvent(content["uuid"].asString().c_str(), "event.system.floorplandeleted", "");
-					} else {
-						reply["returncode"] = -1;
-					}
-					isHandled = true;
-				}
-				if (isHandled) { 
-					Message response;
-					encode(reply, response);
-					replyMessage(message.getReplyTo(), response);
-				}
-			}
-
-		} catch(const NoMessageAvailable& error) {
-			
-		} catch(const std::exception& error) {
-			clog << agocontrol::kLogCrit << "Unhandled exception: " << error.what() << std::endl;
-			if (session.hasError()) {
-				clog << agocontrol::kLogCrit << "Session has error, recreating" << std::endl;
-				session.close();
-				session = agoConnection.createSession(); 
-				receiver = session.createReceiver("agocontrol; {create: always, node: {type: topic}}"); 
-				sender = session.createSender("agocontrol; {create: always, node: {type: topic}}"); 
-			}
-		}
-	}
-
+        return agoConnection->sendMessage(eventType, content);
 }
 
 // helper to determine last element
@@ -367,3 +106,184 @@ void handleEvent(Variant::Map *device, string subject, Variant::Map *content) {
 		(*values)[quantity] = value;
 	}
 }
+
+qpid::types::Variant::Map commandHandler(qpid::types::Variant::Map content) {
+	qpid::types::Variant::Map returnval;
+	std::string internalid = content["internalid"].asString();
+	Variant::Map reply;
+	if (internalid == "agocontroller") {
+		bool isHandled = false;
+		if (content["command"] == "setroomname") {
+			string uuid = content["uuid"];
+			// if no uuid is provided, we need to generate one for a new room
+			if (uuid == "") uuid = generateUuid();
+			inv->setroomname(uuid, content["name"]);
+			reply["uuid"] = uuid;
+			reply["returncode"] = 0;
+			isHandled = true;
+			emitNameEvent(uuid.c_str(), "event.system.roomnamechanged", content["name"].asString().c_str());
+		} else if (content["command"] == "setdeviceroom") {
+			if ((content["uuid"].asString() != "") && (inv->setdeviceroom(content["uuid"], content["room"]) == 0)) {
+				reply["returncode"] = 0;
+				// update room in local device map
+				Variant::Map *device;
+				string room = inv->getdeviceroom(content["uuid"]);
+				string uuid = content["uuid"];
+				device = &inventory[uuid].asMap();
+				(*device)["room"]= room;
+			} else {
+				reply["returncode"] = -1;
+			}
+			isHandled = true;
+		} else if (content["command"] == "setdevicename") {
+			if ((content["uuid"].asString() != "") && (inv->setdevicename(content["uuid"], content["name"]) == 0)) {
+				reply["returncode"] = 0;
+				// update name in local device map
+				Variant::Map *device;
+				string name = inv->getdevicename(content["uuid"]);
+				string uuid = content["uuid"];
+				device = &inventory[uuid].asMap();
+				(*device)["name"]= name;
+				emitNameEvent(content["uuid"].asString().c_str(), "event.system.devicenamechanged", content["name"].asString().c_str());
+			} else {
+				reply["returncode"] = -1;
+			}
+			isHandled = true;
+
+		} else if (content["command"] == "deleteroom") {
+			if (inv->deleteroom(content["uuid"]) == 0) {
+				string uuid = content["uuid"].asString();
+				emitNameEvent(uuid.c_str(), "event.system.roomdeleted", "");
+				reply["returncode"] = 0;
+			} else {
+				reply["returncode"] = -1;
+			}
+			isHandled = true;
+		} else if (content["command"] == "setfloorplanname") {
+			string uuid = content["uuid"];
+			// if no uuid is provided, we need to generate one for a new floorplan
+			if (uuid == "") uuid = generateUuid();
+			if (inv->setfloorplanname(uuid, content["name"]) == 0) {
+				reply["uuid"] = uuid;
+				reply["returncode"] = 0;
+				emitNameEvent(content["uuid"].asString().c_str(), "event.system.floorplannamechanged", content["name"].asString().c_str());
+			} else {
+				reply["returncode"] = -1;
+			}
+			isHandled = true;
+		} else if (content["command"] == "setdevicefloorplan") {
+			if ((content["uuid"].asString() != "") && (inv->setdevicefloorplan(content["uuid"], content["floorplan"], content["x"], content["y"]) == 0)) {
+				reply["returncode"] = 0;
+				emitFloorplanEvent(content["uuid"].asString().c_str(), "event.system.floorplandevicechanged", content["floorplan"].asString().c_str(), content["x"], content["y"]);
+			} else {
+				reply["returncode"] = -1;
+			}
+			isHandled = true;
+
+		} else if (content["command"] == "deletefloorplan") {
+			if (inv->deletefloorplan(content["uuid"]) == 0) {
+				reply["returncode"] = 0;
+				emitNameEvent(content["uuid"].asString().c_str(), "event.system.floorplandeleted", "");
+			} else {
+				reply["returncode"] = -1;
+			}
+			isHandled = true;
+		}
+		if (isHandled) { 
+			/* Message response;
+			encode(reply, response);
+			replyMessage(message.getReplyTo(), response);
+			*/
+		}
+	} else {
+		if (content["command"] == "inventory") {
+			cout << "responding to inventory request" << std::endl;
+			reply["inventory"] = inventory;
+			reply["schema"] = schema;	
+			reply["rooms"] = inv->getrooms();
+			reply["floorplans"] = inv->getfloorplans();
+			reply["system"] = system;
+			reply["returncode"] = 0;
+			/* Message response;
+			encode(reply, response);
+			replyMessage(message.getReplyTo(), response); */
+		}
+	}
+	return reply;
+}
+
+void eventHandler(std::string subject, qpid::types::Variant::Map content) {
+	if (subject == "event.device.announce") {
+		string uuid = content["uuid"];
+		if (uuid != "") {
+			// clog << agocontrol::kLogDebug << "preparing device: uuid="  << uuid << std::endl;
+			Variant::Map device;
+			Variant::Map values;
+			device["devicetype"]=content["devicetype"].asString();
+			device["internalid"]=content["internalid"].asString();
+			device["handled-by"]=content["handled-by"].asString();
+			// clog << agocontrol::kLogDebug << "getting name from inventory" << endl;
+			device["name"]=inv->getdevicename(content["uuid"].asString());
+			device["name"].setEncoding("utf8");
+			// clog << agocontrol::kLogDebug << "getting room from inventory" << endl;
+			device["room"]=inv->getdeviceroom(content["uuid"].asString()); 
+			device["room"].setEncoding("utf8");
+			device["state"]="0";
+			device["state"].setEncoding("utf8");
+			device["values"]=values;
+			cout << "adding device: uuid="  << uuid  << " type: " << device["devicetype"].asString() << std::endl;
+			// clog << agocontrol::kLogDebug << "adding device: uuid="  << uuid  << " type: " << device["devicetype"].asString() << std::endl;
+			inventory[uuid] = device;
+		}
+	} else if (subject == "event.device.remove") {
+		string uuid = content["uuid"];
+		if (uuid != "") {
+			// clog << agocontrol::kLogDebug << "removing device: uuid="  << uuid  << std::endl;
+			Variant::Map::iterator it = inventory.find(uuid);
+			if (it != inventory.end()) {
+				inventory.erase(it);
+			}
+		}
+	} else {
+		if (content["uuid"].asString() != "") {
+			string uuid = content["uuid"];
+			// see if we have that device in the inventory already, if yes handle the event
+			if (inventory.find(uuid) != inventory.end()) handleEvent(&inventory[uuid].asMap(), subject, &content);
+		}
+
+	}
+}
+
+int main(int argc, char **argv) {
+//	clog.rdbuf(new agocontrol::Log("agoresolver", LOG_LOCAL0));
+
+	agoConnection = new AgoConnection("resolver");
+	agoConnection->addHandler(commandHandler);
+	agoConnection->addEventHandler(eventHandler);
+	agoConnection->setFilter(false);
+
+	string schemafile;
+
+//	clog << agocontrol::kLogNotice << "starting up" << std::endl;
+
+	schemafile=getConfigOption("system", "schema", "/etc/opt/agocontrol/schema.yaml");
+
+	system["uuid"] = getConfigOption("system", "uuid", "00000000-0000-0000-000000000000");
+	system["version"] = AGOCONTROL_VERSION;
+
+//	clog << agocontrol::kLogDebug << "parsing schema file" << std::endl;
+	schema = parseSchema(schemafile.c_str());
+
+//	clog << agocontrol::kLogDebug << "reading inventory" << std::endl;
+	inv = new Inventory("/etc/opt/agocontrol/inventory.db");
+
+	agoConnection->addDevice("agocontroller","agocontroller");
+	
+	// discover devices
+//	clog << agocontrol::kLogDebug << "discovering devices" << std::endl;
+	Variant::Map discovercmd;
+	discovercmd["command"] = "discover";
+	agoConnection->sendMessage("",discovercmd);
+	agoConnection->run();	
+}
+
