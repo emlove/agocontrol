0a9eb42ca865c341dab24037bb242a05e1543217
Christoph Jaeger <christoph@agocontrol.com>
2013-09-05 19:32:09 +0000

    add index to timestamp and environment

--- data/datalogger.sql
+++ data/datalogger.sql
@@ -1,4 +1,6 @@
 PRAGMA foreign_keys=OFF;
 BEGIN TRANSACTION;
 CREATE TABLE data(id INTEGER PRIMARY KEY AUTOINCREMENT, uuid TEXT, environment TEXT, unit TEXT, level REAL, timestamp TIMESTAMP);
+CREATE INDEX timestamp_idx ON data(timestamp);
+CREATE INDEX environment_idx ON data(environment);
 COMMIT;
