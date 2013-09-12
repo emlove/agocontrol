21f43e36a029d659dfb4c20cde440813646f54df
Christoph Jaeger <christoph@agocontrol.com>
2013-09-12 20:49:37 +0000

    try fix tab nav in configuration - not working now

--- core/rpc/html/js/app.configuration.js
+++ core/rpc/html/js/app.configuration.js
@@ -4,7 +4,24 @@
  */
 function configuration() {
     this.hasNavigation = ko.observable(true);
+
 }
+
+
+$('.tab-nav li > a').click(function() {
+        $(".tab-nav li").each(function() {
+            $(this).removeClass('active');
+        });
+
+        $(this).parent().toggleClass('active');
+
+        this.$el = $('.tabs');
+        var index = $(this).parent().index();
+        this.$content = this.$el.find(".tab-content");
+        this.$nav = $(this).parent().find('li');
+        this.$nav.add(this.$content).removeClass("active");
+        this.$nav.eq(index).add(this.$content.eq(index)).addClass("active");
+});
 
 /**
  * Initalizes the Configuration model
