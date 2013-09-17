ec13adde40aab4bd342f7f3b7b5c233deebf4c8a
Harald Klein <hari@vt100.at>
2013-09-17 15:56:33 +0200

    adjust versioning for git repo

--- debian/version-increment.sh
+++ debian/version-increment.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-dch -v ${1} "new package"
+dch -i "new package from ${1}"
 dch -r "unstable"
-echo "#define AGOCONTROL_VERSION \"${1}\"" > version.h
+VERSION=$(dpkg-parsechangelog  | grep ^Version | sed 's/^Version: //g')
+echo "#define AGOCONTROL_VERSION \"${VERSION}\"" > version.h
