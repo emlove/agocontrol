b873400a82fa5df5255925090b95e74bd10af980
Harald Klein <hari@vt100.at>
2013-09-17 20:50:43 +0200

    prepend 1.0 to version

--- debian/version-increment.sh
+++ debian/version-increment.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 NUMCOMMIT=$(git rev-list --count HEAD)
-dch -v ${NUMCOMMIT} "new package from ${1}"
+dch -v 1.0-${NUMCOMMIT} "new package from ${1}"
 dch -r "unstable"
 VERSION=$(dpkg-parsechangelog  | grep ^Version | sed 's/^Version: //g')
-echo "#define AGOCONTROL_VERSION \"${VERSION}-${NUMCOMMIT}\"" > version.h
+echo "#define AGOCONTROL_VERSION \"${VERSION}\"" > version.h
