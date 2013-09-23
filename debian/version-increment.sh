8c1e104b2cf9fbfa293b4445d53374f96a671097
Harald Klein <hari@vt100.at>
2013-09-23 20:52:23 +0200

    add git short commit id to package version

--- debian/version-increment.sh
+++ debian/version-increment.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 NUMCOMMIT=$(git rev-list --count HEAD)
-dch -v 1.0-${NUMCOMMIT} "new package from ${1}"
+GITSHORT=$(git rev-parse --short HEAD)
+dch -v 1.0-${NUMCOMMIT}-${GITSHORT} "new package from ${1}"
 dch -r "unstable"
 VERSION=$(dpkg-parsechangelog  | grep ^Version | sed 's/^Version: //g')
 echo "#define AGOCONTROL_VERSION \"${VERSION}\"" > version.h
