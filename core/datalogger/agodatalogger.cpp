bfec83f77a8387883c4893b3945eec794d67730d
Adel <adel@agocontrol.com>
2013-09-30 11:50:08 +0200

    datalogger_cpp: Implement getloggergraph

--- core/datalogger/agodatalogger.cpp
+++ core/datalogger/agodatalogger.cpp
@@ -14,7 +14,6 @@
 #include <boost/date_time/posix_time/time_parsers.hpp>
 #include <boost/date_time/posix_time/posix_time_types.hpp>
 
-
 #include "agoclient.h"
 
 #ifndef DBFILE
@@ -30,6 +29,9 @@ qpid::types::Variant::Map inventory;
 
 std::string uuidToName(std::string uuid) {
 	qpid::types::Variant::Map devices = inventory["inventory"].asMap();
+	if (devices[uuid].isVoid()) {
+        return "";
+    }
 	qpid::types::Variant::Map device = devices[uuid].asMap();
 	return device["name"].asString() == "" ? uuid : device["name"].asString();
 }
@@ -50,7 +52,7 @@ void eventHandler(std::string subject, qpid::types::Variant::Map content) {
 
 		string query = "INSERT INTO data VALUES(null, ?, ?, ?, ?)";
 		rc = sqlite3_prepare_v2(db, query.c_str(), -1, &stmt, NULL);
-		if(rc!=SQLITE_OK) {
+		if(rc != SQLITE_OK) {
 			fprintf(stderr, "sql error #%d: %s\n", rc,sqlite3_errmsg(db));
 			return;
 		}
@@ -75,23 +77,67 @@ void eventHandler(std::string subject, qpid::types::Variant::Map content) {
 }
 
 void GetGraphData(qpid::types::Variant::Map content, qpid::types::Variant::Map &result) {
-    printf("start is: %s\n", content["start"].asString().c_str());
+    sqlite3_stmt *stmt;
+    int rc;
+    qpid::types::Variant::List values;
+
+    // Parse the timestrings
+    string startDate = content["start"].asString();
+    string endDate = content["end"].asString();
+    replaceString(startDate, "-", "");
+    replaceString(startDate, ":", "");
+    replaceString(startDate, "Z", "");
+    replaceString(endDate, "-", "");
+    replaceString(endDate, ":", "");
+    replaceString(endDate, "Z", "");
+    
     boost::posix_time::ptime base(boost::gregorian::date(1970, 1, 1));
-    boost::posix_time::time_duration start = boost::posix_time::time_from_string(content["start"].asString()) - base;
+    boost::posix_time::time_duration start = boost::posix_time::from_iso_string(startDate) - base;
     printf("%d\n", start.total_seconds());
     
+    boost::posix_time::time_duration end = boost::posix_time::from_iso_string(endDate) - base;
+    printf("%d\n", end.total_seconds());
     
+    rc = sqlite3_prepare_v2(db, "SELECT timestamp, level FROM data WHERE timestamp BETWEEN ? AND ?  AND environment = ? AND uuid = ? ORDER BY timestamp", -1, &stmt, NULL);
+	if(rc != SQLITE_OK) {
+		fprintf(stderr, "sql error #%d: %s\n", rc,sqlite3_errmsg(db));
+		return;
+	}
+
+	sqlite3_bind_int(stmt, 1, start.total_seconds());
+	sqlite3_bind_int(stmt, 2, end.total_seconds());
+	sqlite3_bind_text(stmt, 3, content["env"].asString().c_str(), -1, NULL);
+	sqlite3_bind_text(stmt, 4, content["deviceid"].asString().c_str(), -1, NULL);
+    
+    do {
+        rc = sqlite3_step(stmt);
+        switch(rc) {
+			case SQLITE_ERROR:
+				fprintf(stderr, "step error: %s\n",sqlite3_errmsg(db));
+				break;
+			case SQLITE_ROW:
+			    qpid::types::Variant::Map value;
+			    value["time"] = sqlite3_column_int(stmt, 0);
+			    value["level"] = sqlite3_column_double(stmt, 1);
+			    values.push_back(value);
+				break;
+		}
+    }
+    while (rc == SQLITE_ROW);
+    
+    sqlite3_finalize(stmt);
+    
+    qpid::types::Variant::Map data;
+    data["values"] = values;
+    result["result"] = data;
 }
 
 qpid::types::Variant::Map commandHandler(qpid::types::Variant::Map content) {
 	qpid::types::Variant::Map returnval;
 	std::string internalid = content["internalid"].asString();
-	printf("WTF!!!"); // FIXME: Never gets called?!?
-	std::cout <<  content["internalid"].asString() << std::endl;
 	if (internalid == "dataloggercontroller") {
 		if (content["command"] == "getloggergraph") {
 		    GetGraphData(content, returnval);
-			// start, end, env, deviceid
         } else if (content["command"] == "getdevieenvironments") {
 
 
@@ -102,7 +148,7 @@ qpid::types::Variant::Map commandHandler(qpid::types::Variant::Map content) {
 
 int main(int argc, char **argv) {
     int rc = sqlite3_open(DBFILE, &db);
-	if( rc != SQLITE_OK ){
+	if( rc != SQLITE_OK){
 		fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
 		sqlite3_close(db);
 		return 1;
