4635963fcc49b627ea04c8fb2edd7a9fea74c414
Harald Klein <hari@vt100.at>
2013-10-02 13:03:49 +0200

    parse config for webcams, use agoclient, return frame as base64 in a dict

--- devices/agowebcam.py
+++ devices/agowebcam.py
@@ -1,137 +1,42 @@
-#! /usr/bin/env python
+#!/usr/bin/python
 
-import random
-import sys
-import syslog
-import time
-import pickle
-import optparse
-import ConfigParser
-import socket
+# ago client webcam device
+#
+# copyright (c) 2013 Harald Klein <hari+ago@vt100.at>
+#
 
+import agoclient
 import urllib2
-from qpid.messaging import *
-from qpid.util import URL
-from qpid.log import enable, DEBUG, WARN
+import base64
 
-config = ConfigParser.ConfigParser()
-config.read('/etc/opt/agocontrol/config.ini')
+client = agoclient.AgoConnection("webcam")
 
-try:
-	username = config.get("system","username")
-except:
-	username = "agocontrol"
 
-try:
-	password = config.get("system","password")
-except:
-	password = "letmein"
+def messageHandler(internalid, content):
+	result = {}
+	result["result"] = -1;
+	if "command" in content:
+		if content['command'] == 'getvideoframe':
+			print "getting video frame"
+			u = urllib2.urlopen(internalid)	
+			buffer = u.read()
+			result["image"] = base64.b64encode(buffer)
+			result["result"] = 0;
+	return result
 
-try:
-	broker = config.get("system","broker")
-except:
-	broker = "localhost"
+client.addHandler(messageHandler)
+devicelist=agoclient.getConfigOption("webcam", "devices", "")
 
 try:
-	debug = config.get("system","debug")
+	devices = map(str, devicelist.split(','))
 except:
-	debug = "WARN"
-
-if debug=="DEBUG":
-	enable("qpid", DEBUG)
+	print "error reading device list"
 else:
-	enable("qpid", WARN)
+	for device in devices:
+		print "announcing device", device
+		if "rtsp://" in device:
+			client.addDevice(device, "onvifnvt")
+		else:
+			client.addDevice(device, "camera")
 
- 
-parser = optparse.OptionParser(usage="usage: %prog <command> [options] [ PARAMETERS ... ]",
-                               description="send automation control commands")
-parser.add_option("-b", "--broker", default=broker, help="hostname of broker (default %default)")
-parser.add_option("-u", "--username", default=username, help="specify a username")
-parser.add_option("-P", "--password", default=password, help="specify a password")
-
-opts, args = parser.parse_args()
-
-# route stderr to syslog
-class LogErr:
-        def write(self, data):
-                syslog.syslog(syslog.LOG_ERR, data)
-
-syslog.openlog(sys.argv[0], syslog.LOG_PID, syslog.LOG_DAEMON)
-sys.stderr = LogErr()
-
-connection = Connection(opts.broker, username=opts.username, password=opts.password,  reconnect=True)
-connection.open()
-session = connection.session()
-receiver = session.receiver("agocontrol; {create: always, node: {type: topic}}")
-sender = session.sender("agocontrol; {create: always, node: {type: topic}}")
-
-def reportdevice(uuid, type='camera', product='IP Camera'):
-	try:
-		content = {}
-		content["devicetype"]=type
-		content["uuid"] = uuid
-		content["product"] = product
-		message = Message(content=content,subject="event.device.announce")
-		sender.send(message)
-	except SendError, e:
-		print e
-
-def sendStateChangedEvent(uuid, level):
-        try:
-                content = {}
-                content["uuid"] = uuid
-                content["level"] = level
-                message = Message(content=content,subject="event.device.statechanged")
-                sender.send(message)
-        except SendError, e:
-                print e
-
-syslog.syslog(syslog.LOG_NOTICE, "agowebcam.py startup")
-
-devices = {}
-devices["2ab0e80d-7b48-4d17-b083-58fda121d6e4"] = "http://192.168.0.90/axis-cgi/jpg/image.cgi?resolution=640x480"
-
-def discovery():
-	for (uuid, url) in devices.iteritems():
-		reportdevice(uuid=uuid)
-
-discovery()
-
-
-while True:
-	try:
-		message = receiver.fetch(timeout=1)
-		if message.content:
-			if 'command' in message.content:
-				print message
-				if message.content['command'] == 'discover':
-					syslog.syslog(syslog.LOG_NOTICE, "discovering devices")
-					discovery()
-				elif message.content['command'] == 'inventory':
-					syslog.syslog(syslog.LOG_NOTICE, "ignoring inventory command")
-				else:
-					if 'uuid' in message.content:
-						for (uuid, url) in devices.iteritems():
-							if message.content['uuid'] == uuid:
-								if message.content['command'] == 'getvideoframe' and message.reply_to:
-									print "getting video frame"
-									u = urllib2.urlopen(url)	
-									buffer = u.read()
-									replysender = session.sender(message.reply_to)
-									response = Message(buffer)
-									response.content_type="image/jpg"
-									try:
-										replysender.send(response)
-									except SendError, e:
-										print "Can't send ACK: ", e
-	except Empty, e:
-		pass
-	except KeyError, e:
-		print "key error in command evaluation", e
-	except ReceiverError, e:
-		print e
-		time.sleep(1)
-
-
-
-
+client.run()
