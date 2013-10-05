16c7d8f9e4112961e24c7b3025118d62cde016d5
Adel <adel@agocontrol.com>
2013-10-05 21:16:26 +0200

    cam: Use url directly for now

--- core/rpc/html/js/app.js
+++ core/rpc/html/js/app.js
@@ -194,15 +194,16 @@ function device(obj, uuid) {
 
     if (this.devicetype == "camera") {
 	this.getVideoFrame = function() {
-	    var content = {};
-	    content.command = "getvideoframe";
-	    content.uuid = self.uuid;
-	    sendCommand(content, function(r) {
-		if (r.result.result.image && document.getElementById("camIMG")) {
-		    document.getElementById("camIMG").src = "data:image/jpeg;base64," + r.result.result.image;
-		    $("#camIMG").show();
-		}
-	    });
+	    // FIXME: Workaround for base64 transfer bug
+	    document.getElementById("camIMG").src = self.internalid + "?" + (new Date()).getTime();
+	    $("#camIMG").show();
+	    /*
+	     * var content = {}; content.command = "getvideoframe"; content.uuid =
+	     * self.uuid; sendCommand(content, function(r) { console.log(r); if
+	     * (r.result.result.image && document.getElementById("camIMG")) {
+	     * document.getElementById("camIMG").src = "data:image/jpeg;base64," +
+	     * r.result.result.image; $("#camIMG").show(); } }, 90);
+	     */
 	};
     }
 
