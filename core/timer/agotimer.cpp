a60829f80df36519b2b92cf83e884185250211dc
Harald Klein <hari@vt100.at>
2014-01-13 00:36:05 +0100

    fix iso weekdays

--- core/timer/agotimer.cpp
+++ core/timer/agotimer.cpp
@@ -53,7 +53,7 @@ void *timer(void *param) {
 		content["month"]=tms->tm_mon+1;
 		content["day"]=tms->tm_mday;
 		content["year"]=tms->tm_year+1900;
-		content["weekday"]=tms->tm_wday+1;
+		content["weekday"]= tms->tm_wday == 0 ? 7 : tms->tm_wday;
 		content["yday"]=tms->tm_yday+1;
 		agoConnection->sendMessage("event.environment.timechanged", content);
 		sleep(2);
