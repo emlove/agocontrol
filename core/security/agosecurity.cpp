c2b5ef41a0f004cf1bc85754b0f7e46929a84b9a
Harald Klein <hari@vt100.at>
2014-01-14 00:58:06 +0100

    introduce securitymap, implement setzones, launch alarmthread

--- core/security/agosecurity.cpp
+++ core/security/agosecurity.cpp
@@ -13,17 +13,27 @@
 
 #include "agoclient.h"
 
+#ifndef SECURITYMAPFILE
+#define SECURITYMAPFILE CONFIG_BASE_DIR "/maps/securitymap.json"
+#endif
+
+
 using namespace qpid::messaging;
 using namespace qpid::types;
 using namespace agocontrol;
 
 AgoConnection *agoConnection;
 std::string agocontroller;
-std::string housemode;
-qpid::types::Variant::Map zonemap;
+qpid::types::Variant::Map securitymap;
 
-void *security(void *param) {
+void *alarmthread(void *param) {
+	int delay = *((int *)param);
 	Variant::Map content;
+	cout << "Alarm triggered, delay: " << delay << endl;
+	while (delay-- > 0) {
+		cout << "count down: " << delay << endl;
+		sleep(1);
+	}
 	agoConnection->sendMessage("event.security.intruderalert", content);
 }
 
@@ -40,27 +50,51 @@ qpid::types::Variant::Map commandHandler(qpid::types::Variant::Map content) {
 	if (internalid == "securitycontroller") {
 		if (content["command"] == "sethousemode") {
 			if (content["mode"].asString() != "") {
-				housemode = content["mode"].asString();
-				cout << "setting mode: " << housemode << endl;
-				agoConnection->setGlobalVariable("housemode", Variant(housemode));
-				returnval["result"] = 0;
+				securitymap["housemode"] = content["mode"].asString();
+				cout << "setting mode: " << content["mode"] << endl;
+				agoConnection->setGlobalVariable("housemode", content["mode"]);
+				if (variantMapToJSONFile(securitymap, SECURITYMAPFILE)) {
+					returnval["result"] = 0;
+				} else {
+					returnval["result"] = -1;
+				}
 			} else {
 				returnval["result"] = -1;
 			}
 
 		} else if (content["command"] == "triggerzone") {
 			std::string zone = content["zone"];
+			qpid::types::Variant::Map zonemap;
+			std::string housemode = securitymap["housemode"];
+			if (!(securitymap["zones"].isVoid())) zonemap = securitymap["zones"].asMap();
 			if (!(zonemap[housemode].isVoid())) {
 				if (zonemap[housemode].getType() == qpid::types::VAR_LIST) {
 					if (findList(zonemap[housemode].asList(), zone)) {
-
+						static pthread_t securityThread;
+						int delay = 30;
+						pthread_create(&securityThread,NULL,alarmthread,(void *)&delay);
 					}
 				}	
 
 			}
-		} else if (content["command"] == "setzones") {
 			returnval["result"] = 0;
-
+		} else if (content["command"] == "setzones") {
+			try {
+				cout << "setzones request" << endl;
+				qpid::types::Variant::Map newzones = content["zonemap"].asMap();
+				cout << "zone content:" << newzones << endl;
+				securitymap["zones"] = newzones;
+				if (variantMapToJSONFile(securitymap, SECURITYMAPFILE)) {
+					returnval["result"] = 0;
+				} else {
+					returnval["result"] = -1;
+				}
+			} catch (qpid::types::InvalidConversion) {
+                                returnval["result"] = -1;
+                        } catch (...) {
+                                returnval["result"] = -1;
+				returnval["error"] = "exception";
+			}
 		} else {
 			returnval["result"] = -1;
 		}
@@ -80,14 +114,25 @@ int main(int argc, char** argv) {
 	openlog(NULL, LOG_PID & LOG_CONS, LOG_DAEMON);
 	agoConnection = new AgoConnection("security");
 
+	securitymap = jsonFileToVariantMap(SECURITYMAPFILE);
+
+	cout << "securitymap: " << securitymap << endl;
+	std::string housemode = securitymap["housemode"];
+	cout << "house mode: " << housemode;
+	agoConnection->setGlobalVariable("housemode", housemode);
+/*
+	qpid::types::Variant::List armedZones;
+	armedZones.push_back("hull");
+	zonemap["armed"] = armedZones;
+
+	securitymap["zones"] = zonemap;
+	variantMapToJSONFile(securitymap, SECURITYMAPFILE);
+*/
 	agoConnection->addDevice("securitycontroller", "securitycontroller");
 	agoConnection->addHandler(commandHandler);
 	agoConnection->addEventHandler(eventHandler);
 
 	int pin=atoi(getConfigOption("security", "pin", "1234").c_str());
-
-	static pthread_t securityThread;
-	pthread_create(&securityThread,NULL,security,NULL);
 
 	agoConnection->run();	
 
