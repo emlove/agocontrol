4e6e0633e1fb740875e149c3696180cfa5965fe2
Adel <adel@agocontrol.com>
2014-02-04 17:38:55 +0100

    Revert "fix bug 165 (http://www.agocontrol.com/trac/ticket/165)"
    This reverts commit 18099df45c68d642f5584f41033bad6e3c9db869.
    
    This is not a fix but an ugly hack, you cannot simply globally replace
    all &amp; with & in the html. If anything use a more specific regex.

--- core/rpc/mergetranslation.sh
+++ core/rpc/mergetranslation.sh
@@ -2,8 +2,6 @@
 TMPFILE=$(mktemp -u).xml.in
 SOURCEFILE=$(realpath $1)
 ln -s ${SOURCEFILE} ${TMPFILE}
-sed -i 's/\&\&/\&amp;\&amp;/g' ${TMPFILE}
 intltool-merge -x html/po ${TMPFILE} $2
-sed -i 's/\&amp;\&amp;/\&\&/g' $2
 sed -i '1d' $2
 rm ${TMPFILE}
