43abb75366470b8663997c42cd5f3c28affbcce1
Harald Klein <hari@vt100.at>
2014-03-12 21:46:55 +0100

    add debug output

--- core/timer/agotimer.cpp
+++ core/timer/agotimer.cpp
@@ -86,7 +86,7 @@ void *suntimer(void *param) {
 				agoConnection->sendMessage("", setvariable);
 				// now wait for sunrise
 				syslog(LOG_NOTICE, "minutes to wait for sunrise: %ld\n",(sunrise-seconds + sunriseoffset)/60);
-				//printf("minutes to wait for sunrise: %ld\n",(sunrise-seconds)/60);
+				printf("sunrise at: %s - minutes to wait for sunrise: %ld\n",asctime(localtime(&sunrise)),(sunrise-seconds+sunriseoffset)/60);
 				// printf("sunrise: %s\n",asctime(localtime(&sunrise)));
 				sleep(sunrise-seconds + sunriseoffset);
 				syslog(LOG_NOTICE, "sending sunrise event");
@@ -95,6 +95,7 @@ void *suntimer(void *param) {
 				setvariable["value"] = false;
 				agoConnection->sendMessage("", setvariable);
 				syslog(LOG_NOTICE, "minutes to wait for sunrise: %ld\n",(sunrise_tomorrow-seconds + sunriseoffset)/60);
+				printf("sunrise at: %s - minutes to wait for sunrise: %ld\n",asctime(localtime(&sunrise_tomorrow)),(sunrise_tomorrow-seconds+sunriseoffset)/60);
 				sleep(sunrise_tomorrow-seconds + sunriseoffset);
 				syslog(LOG_NOTICE, "sending sunrise event");
 				agoConnection->sendMessage("event.environment.sunrise", content);
@@ -102,6 +103,7 @@ void *suntimer(void *param) {
 				setvariable["value"] = true;
 				agoConnection->sendMessage("", setvariable);
 				syslog(LOG_NOTICE, "minutes to wait for sunset: %ld\n",(sunset-seconds+ sunsetoffset)/60);
+				printf("sunset at: %s - minutes to wait for sunset: %ld\n",asctime(localtime(&sunset)),(sunset-seconds+sunsetoffset)/60);
 				sleep(sunset-seconds+sunsetoffset);
 				syslog(LOG_NOTICE, "sending sunset event");
 				agoConnection->sendMessage("event.environment.sunset", content);
