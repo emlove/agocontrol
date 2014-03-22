0ee8eabac67d118c6500a2c031a8bc5fcc622060
Johan Ström <johan@stromnet.se>
2014-03-22 21:06:52 +0100

    FreeBSD build fixes for new CMake, and some other
    jsoncpp: normalize rpc to include from jsoncpp/json/ instead of just json/, following the same
    	pattern as in shared/agoclient.h
    Let root CMake-file resolve jsoncpp details, jsoncpp is used everywhere (via agoclient.h)
    
    Dirs: FreeBSD wants stuff in /usr/local and nowhere else.
    
    Use /usr/local/(include|libs) in all targets, needed for uuid, jsoncpp and more.

--- core/rpc/mergetranslation.sh
+++ core/rpc/mergetranslation.sh
@@ -3,5 +3,13 @@ TMPFILE=$(mktemp -u).xml.in
 SOURCEFILE=$(realpath $1)
 ln -s ${SOURCEFILE} ${TMPFILE}
 intltool-merge -x html/po ${TMPFILE} $2
-sed -i '1d' $2
+
+# Remove first line: <?xml version...
+# sed -i is not POSIX, and behaves differently on BSD vs GNU
+if [ `uname -s` == "FreeBSD" ]; then
+	sed -i '' '1d' $2
+else
+	sed -i '1d' $2
+fi
+
 rm ${TMPFILE}
